d8903feb8a20c23ebcc71fda7aa6729fd27815fb
Josh Pigford <josh@joshpigford.com>
2024-01-01 19:51:40 -0600

    Application shell

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  
+
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  root "pages#home"
+  root "pages#dashboard"
 end
