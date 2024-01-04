7dbfcd5c0c9f384761c5aa2e27a59bf71bbe0955
Josh Pigford <josh@joshpigford.com>
2024-01-04 13:59:03 -0600

    Missed one Polygon bit

--- app/helpers/holdings_helper.rb
+++ app/helpers/holdings_helper.rb
@@ -4,7 +4,6 @@ module HoldingsHelper
     padding = size < 10 ? 1 : 2
 
     return get_svg_logo(security, size, rounded_size, padding) if security.logo_svg.present?
-    return get_polygon_logo(security, size, rounded_size, padding) if security.logo.present? && security.logo_source == 'polygon'
     return get_image_logo(security, size, rounded_size) if security.logo.present?
     
     get_text_logo(security, size, rounded_size, padding)
@@ -13,11 +12,6 @@ module HoldingsHelper
   def get_svg_logo(security, size, rounded_size, padding)
     logo = process_svg(security.logo_svg)
     raw "<div class='flex items-center justify-center w-#{size} h-#{size} #{rounded_size}' style='background-color:#{security.logo_colors['color1']}'><div class='p-#{padding + 1}'>#{logo}</div></div>"
-  end
-  
-  def get_polygon_logo(security, size, rounded_size, padding)
-    logo = image_tag("#{security.logo}?apiKey=#{ENV['POLYGON_KEY']}", class: 'object-contain w-full h-full')
-    raw "<div class='flex items-center justify-center bg-gray-200 w-#{size} h-#{size} #{rounded_size}'><div class='p-#{padding}'>#{logo}</div></div>"
   end
   
   def get_image_logo(security, size, rounded_size)
