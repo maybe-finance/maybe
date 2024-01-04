9090769c65a0ae3155d40f759e05802575aa600b
Josh Pigford <josh@joshpigford.com>
2024-01-04 14:10:17 -0600

    Sentry update

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,5 +1,5 @@
 Sentry.init do |config|
-  config.dsn = 'https://1789da52e499454c8eb0b2d570a7cb56@o675109.ingest.sentry.io/4504980729364480'
+  config.dsn = ENV['SENTRY_DSN']
   config.breadcrumbs_logger = [:active_support_logger, :http_logger]
   config.enabled_environments = %w[production]
 
@@ -7,8 +7,4 @@ Sentry.init do |config|
   # of transactions for performance monitoring.
   # We recommend adjusting this value in production.
   config.traces_sample_rate = 1.0
-  # or
-  config.traces_sampler = lambda do |context|
-    true
-  end
 end
