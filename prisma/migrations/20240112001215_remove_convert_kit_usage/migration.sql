c3d9eeb8f57f29e5e0f5ab0c9668fa4df4b09b08
Michaël De Boey <info@michaeldeboey.be>
2024-01-12 00:56:02 +0100

    fix: add ConvertKit removal migration

---
+++ prisma/migrations/20240112001215_remove_convert_kit_usage/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `convert_kit_id` on the `user` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "user" DROP COLUMN "convert_kit_id";
