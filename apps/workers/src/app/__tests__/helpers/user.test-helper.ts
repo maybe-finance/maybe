55121eba081797651f279366e19a1c56bd8c3ac8
Anthony Shew <anthonyshew@gmail.com>
2024-01-14 22:52:43 -0700

    Handle types and tests.

--- apps/workers/src/app/__tests__/helpers/user.test-helper.ts
+++ apps/workers/src/app/__tests__/helpers/user.test-helper.ts
@@ -1,12 +1,9 @@
 import type { PrismaClient, User } from '@prisma/client'
 
-export async function resetUser(
-    prisma: PrismaClient,
-    auth0Id = 'auth0|workers-integration-test-id'
-): Promise<User> {
+export async function resetUser(prisma: PrismaClient, authId = 'TODO'): Promise<User> {
     // eslint-disable-next-line
     const [_, __, ___, user] = await prisma.$transaction([
-        prisma.$executeRaw`DELETE FROM "user" WHERE auth0_id=${auth0Id};`,
+        prisma.$executeRaw`DELETE FROM "user" WHERE auth_id=${authId};`,
 
         // Deleting a user does not cascade to securities, so delete all security records
         prisma.$executeRaw`DELETE from security;`,
@@ -14,7 +11,7 @@ export async function resetUser(
 
         prisma.user.create({
             data: {
-                auth0Id,
+                authId,
                 email: 'test@example.com',
                 finicityCustomerId: 'TEST',
             },
