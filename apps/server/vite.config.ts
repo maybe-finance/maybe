553e7380d849757ca6b61689dbc78dbbd8418088
David Neuman <david.m.neuman@gmail.com>
2024-01-15 22:42:41 -0500

    Setup server proxy

--- apps/server/vite.config.ts
+++ apps/server/vite.config.ts
@@ -2,11 +2,22 @@
 import { defineConfig } from 'vite'
 
 import viteTsConfigPaths from 'vite-tsconfig-paths'
+import env from './src/env'
 
 export default defineConfig({
     server: {
-        port: 4200,
+        port: Number(env.NX_PORT),
         host: 'localhost',
+        proxy: {
+            '/v1': {
+                target: `http://localhost:${env.NX_PORT}`,
+                changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/v1/, ''),
+            },
+        },
+    },
+    optimizeDeps: {
+        exclude: ['mock-aws-s3', 'aws-sdk'],
     },
 
     plugins: [
