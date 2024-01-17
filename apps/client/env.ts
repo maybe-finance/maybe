5117991c1677cb726fe28e022529cdd1bc96ee4f
Six <23470032+6ixfalls@users.noreply.github.com>
2024-01-17 10:43:42 -0800

    Fix getEnv

--- apps/client/env.ts
+++ apps/client/env.ts
@@ -8,7 +8,7 @@ function isBrowser() {
     return Boolean(typeof window !== "undefined" && window.__appenv);
 }
 
-function env(key: string) {
+function getEnv(key: string) {
     if (!key.length) {
       throw new Error('No env key provided');
     }
@@ -21,10 +21,10 @@ function env(key: string) {
 }
 
 const env = {
-    NEXT_PUBLIC_API_URL: env("NEXT_PUBLIC_API_URL") || 'http://localhost:3333',
-    NEXT_PUBLIC_LD_CLIENT_SIDE_ID: env("process.env.NEXT_PUBLIC_LD_CLIENT_SIDE_ID") || 'REPLACE_THIS',
-    NEXT_PUBLIC_SENTRY_DSN: env("process.env.NEXT_PUBLIC_SENTRY_DSN"),
-    NEXT_PUBLIC_SENTRY_ENV: env("process.env.NEXT_PUBLIC_SENTRY_ENV"),
+    NEXT_PUBLIC_API_URL: getEnv("NEXT_PUBLIC_API_URL") || 'http://localhost:3333',
+    NEXT_PUBLIC_LD_CLIENT_SIDE_ID: getEnv("NEXT_PUBLIC_LD_CLIENT_SIDE_ID") || 'REPLACE_THIS',
+    NEXT_PUBLIC_SENTRY_DSN: getEnv("NEXT_PUBLIC_SENTRY_DSN"),
+    NEXT_PUBLIC_SENTRY_ENV: getEnv("NEXT_PUBLIC_SENTRY_ENV"),
 }
 
 export default env
