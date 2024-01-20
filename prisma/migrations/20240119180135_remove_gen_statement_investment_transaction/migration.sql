d4984fba311e1ea50072d7f472c2cc6af3f4f96a
Tyler Myracle <tyler.myracle@gmail.com>
2024-01-19 19:27:22 -0600

    revert changes to schema/db and undo changes to account integration spec

--- prisma/migrations/20240119180135_remove_gen_statement_investment_transaction/migration.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE
-  "investment_transaction" DROP COLUMN "category";
-
-ALTER TABLE
-  "investment_transaction"
-ADD
-  COLUMN "category" "InvestmentTransactionCategory" DEFAULT 'other' :: "InvestmentTransactionCategory" NOT NULL;
