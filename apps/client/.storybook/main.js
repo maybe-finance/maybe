7f4e976c529face3ad6ddd7f258205c04b27fbe6
Simon Bukin <8992420+simonbukin@users.noreply.github.com>
2024-01-20 16:20:41 -0800

    Add .babelrc to fix build issues

--- apps/client/.storybook/main.js
+++ apps/client/.storybook/main.js
@@ -2,7 +2,6 @@ const rootMain = require('../../../.storybook/main')
 
 module.exports = {
     ...rootMain,
-
     core: { ...rootMain.core, builder: 'webpack5' },
     stories: ['../**/*.stories.@(js|jsx|ts|tsx)'],
     addons: [...rootMain.addons, '@nrwl/react/plugins/storybook'],
--- apps/client/components/Maintenance.stories.tsx
+++ apps/client/components/Maintenance.stories.tsx
@@ -1,11 +1,18 @@
 import type { Story, Meta } from '@storybook/react'
-import Maintenance from './Maintenance'
+import Maintenance from './Maintenance.tsx'
+import React from 'react'
 
 export default {
     title: 'components/Maintenance.tsx',
     component: Maintenance,
 } as Meta
 
-const Template: Story = (args) => <Maintenance />
+const Template: Story = () => {
+    return (
+        <>
+            <Maintenance />
+        </>
+    )
+}
 
 export const Base = Template.bind({})
