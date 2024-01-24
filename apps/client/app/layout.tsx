d97d9cd099b7f8d2e2b49e22f9a99be6e24de210
Aditya Raj <rajaditya27145@gmail.com>
2024-01-24 11:25:40 +0530

    upgrade next & add a root layout

---
+++ apps/client/app/layout.tsx
@@ -0,0 +1,12 @@
+export default function RootLayout({
+    // This will be populated with nested layouts or pages
+    children,
+}: {
+    children: React.ReactNode
+}) {
+    return (
+        <html lang="en">
+            <body>{children}</body>
+        </html>
+    )
+}
