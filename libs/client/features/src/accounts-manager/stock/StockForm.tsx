9b4d109df2b818ee0987194de7ff2f4d3bbb9e93
Karan Handa <karan.handa.fox@gmail.com>
2024-01-24 16:57:02 +0530

    add some STOCKTODOs

--- libs/client/features/src/accounts-manager/stock/StockForm.tsx
+++ libs/client/features/src/accounts-manager/stock/StockForm.tsx
@@ -4,6 +4,7 @@ import { DateUtil } from '@maybe-finance/shared'
 import { useForm } from 'react-hook-form'
 import { AccountValuationFormFields } from '../AccountValuationFormFields'
 
+// STOCKTODO - Change CreateVehicleFields and UpdateVehicleFields
 type Props =
     | {
           mode: 'create'
@@ -16,7 +17,7 @@ type Props =
           onSubmit(data: UpdateVehicleFields): void
       }
 
-export default function VehicleForm({ mode, defaultValues, onSubmit }: Props) {
+export default function StockForm({ mode, defaultValues, onSubmit }: Props) {
     const {
         register,
         control,
@@ -32,10 +33,11 @@ export default function VehicleForm({ mode, defaultValues, onSubmit }: Props) {
     const currentBalanceEditable = !startDate || !DateUtil.isToday(startDate)
 
     return (
-        <form onSubmit={handleSubmit(onSubmit)} data-testid="vehicle-form">
+        <form onSubmit={handleSubmit(onSubmit)} data-testid="stock-form">
             <section className="space-y-4 mb-8">
                 <h6 className="text-white uppercase">Details</h6>
                 <div className="space-y-4">
+                    {/* STOCKTODO - Change this to a drop down where a pre-existing stock-account can be selected or a new stock account can be created*/}
                     <Input
                         type="text"
                         label="Make"
@@ -44,6 +46,7 @@ export default function VehicleForm({ mode, defaultValues, onSubmit }: Props) {
                         {...register('make', { required: true })}
                     />
 
+                    {/* STOCKTODO - Change to to a drop down where all the stocks will be listed and can be chosen by their ticker names */}
                     <Input
                         type="text"
                         label="Model"
@@ -58,6 +61,7 @@ export default function VehicleForm({ mode, defaultValues, onSubmit }: Props) {
                 <section className="space-y-4">
                     <h6 className="text-white uppercase">Valuation</h6>
                     <div>
+                        {/* STOCKTODO - Figure out a way to get the necessary properties here */}
                         <AccountValuationFormFields
                             control={control}
                             currentBalanceEditable={currentBalanceEditable}
