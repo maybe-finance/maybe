43c25aa18d1e3089aecb845ab6774196e3421807
Karan Handa <karan.handa.fox@gmail.com>
2024-01-24 12:23:21 +0530

    add stock files

---
+++ libs/client/features/src/accounts-manager/stock/AddStock.tsx
@@ -0,0 +1,5 @@
+import type { CreateVehicleFields } from '@maybe-finance/client/shared'
+
+export function AddStock({ defaultValues }: { defaultValues: Partial<CreateVehicleFields> }) {
+    return <>{defaultValues}</>
+}
---
+++ libs/client/features/src/accounts-manager/stock/index.ts
@@ -0,0 +1,2 @@
+export * from './AddStock'
+export * from './EditStock'
