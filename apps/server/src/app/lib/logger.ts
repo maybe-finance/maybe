193798fbdcbb48503bf4aea084d59072547bffe0
Karan Handa <karan.handa.fox@gmail.com>
2024-01-25 21:55:05 +0530

    feat: define logger from env variables

--- apps/server/src/app/lib/logger.ts
+++ apps/server/src/app/lib/logger.ts
@@ -1,7 +1,7 @@
 import { createLogger } from '@maybe-finance/server/shared'
 
 const logger = createLogger({
-    level: 'info',
+    level: process.env.DEBUG ?? 'info',
 })
 
 export default logger
