391b2633386d5b3cc7c38932580513b270f5b215
anoop <anoopano20@gmail.com>
2024-01-27 22:40:16 +0530

    data-editor stories

---
+++ apps/client/pages/data-editor.stories.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import type { Story, Meta } from '@storybook/react'
+import DataEditor from './data-editor'
+
+export default {
+    title: 'pages/DataEditor',
+    component: DataEditor,
+} as Meta
+
+export const Default: Story = () => <DataEditor />
