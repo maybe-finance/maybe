575c3a99600928e859de35e7419fd5c28a240da1
anoop <anoopano20@gmail.com>
2024-01-28 13:35:30 +0530

    added new storybook addon

--- apps/client/.storybook/preview.js
+++ apps/client/.storybook/preview.js
@@ -1,9 +1,13 @@
 import '../styles.css'
+import { RouterContext } from 'next/dist/shared/lib/router-context'
 
 import theme from './theme'
 
 export const parameters = {
     docs: {
         theme,
+    },
+    nextRouter: {
+        Provider: RouterContext.Provider,
     },
 }
