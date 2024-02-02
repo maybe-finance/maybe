d6291b6cbee4a8482e3c0d02816683649f206098
Rob Zolkos <rob@zolkos.com>
2024-02-02 16:33:08 +0000

    Mark tests pending
    These are scaffolded placeholder tests anyway.  Once reale features are
    added the skips will be removed.

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -1,6 +1,10 @@
 require "test_helper"
 
 class PagesControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    skip "Pending"
+  end
+
   test "should get index" do
     get pages_index_url
     assert_response :success
