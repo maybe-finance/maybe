129daa6a119327a2eae2e093c048e79c589e9cdb
Rob Zolkos <rob@zolkos.com>
2024-02-02 21:25:53 +0000

    Add option to association

--- app/models/account.rb
+++ app/models/account.rb
@@ -3,7 +3,7 @@ class Account < ApplicationRecord
 
   belongs_to :family
 
-  delegated_type :accountable, types: %w[ Credit Depository Investment Loan OtherAsset OtherLiability Property Vehicle]
+  delegated_type :accountable, types: %w[ Credit Depository Investment Loan OtherAsset OtherLiability Property Vehicle], dependant: :destroy
 
   scope :depository, -> { where(type: "Depository") }
 end
