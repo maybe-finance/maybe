7f2988b7f0536882ab908d6fb72341b91e2e9edb
Rob Zolkos <rob@zolkos.com>
2024-02-02 21:26:20 +0000

    Add index

--- db/migrate/20240202191746_add_accountable_to_account.rb
+++ db/migrate/20240202191746_add_accountable_to_account.rb
@@ -2,5 +2,6 @@ class AddAccountableToAccount < ActiveRecord::Migration[7.2]
   def change
     add_column :accounts, :accountable_type, :string
     add_column :accounts, :accountable_id, :uuid
+    add_index :accounts, :accountable_type
   end
 end
