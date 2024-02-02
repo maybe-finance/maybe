787f0d864eaade206aeb2abb7fd6d7e299a072cf
Rob Zolkos <rob@zolkos.com>
2024-02-02 17:15:12 +0000

    Fix lint issue

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,7 +1,7 @@
 class Account < ApplicationRecord
   VALID_ACCOUNT_TYPES = %w[Investment Depository Credit Loan Property Vehicle OtherAsset OtherLiability].freeze
-  
+
   belongs_to :family
 
-  scope :depository, -> { where(type: 'Depository') }
+  scope :depository, -> { where(type: "Depository") }
 end
