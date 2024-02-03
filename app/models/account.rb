22ad01301e21a8e309c93302ba3aaa59f484a698
Rob Zolkos <rob@zolkos.com>
2024-02-03 15:38:52 -0500

    Remove styling configuration for account types out (#284)
    * Remve styling configuration for account types out
    
    * No need to tailwind config for models

--- app/models/account.rb
+++ app/models/account.rb
@@ -3,12 +3,5 @@ class Account < ApplicationRecord
 
   delegated_type :accountable, types: %w[ Account::Credit Account::Depository Account::Investment Account::Loan Account::OtherAsset Account::OtherLiability Account::Property Account::Vehicle], dependent: :destroy
 
-  delegate :icon, :type_name, :color, to: :accountable
-
-  # Class method to get a representative instance of each accountable type
-  def self.accountable_type_instances
-    accountable_types.map do |type|
-      type.constantize.new
-    end
-  end
+  delegate :type_name, to: :accountable
 end
