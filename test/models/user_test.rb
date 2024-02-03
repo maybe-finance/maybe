663c4d2078aff9ce751d7e09a07e2297eaef8d8c
Lee McAlilly <leemc@hey.com>
2024-02-03 17:14:22 +0000

    Add initial user fixture

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,18 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:bob)
+    @potential_user = User.new(
+                            email: "david@example.com",
+                            password: "foobar",
+                            first_name: "David",
+                            last_name: "Bowie"
+                          )
+  end
+
+  test "should be valid" do
+    assert @user.valid?, @user.errors.full_messages.to_sentence
+  end
+
 end
