e7d7c70b2f74ab14fd4d65ccaf8a2ae08fd64b49
Rob Zolkos <rob@zolkos.com>
2024-02-03 03:09:54 +0000

    Remove account type column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_02_02_230325) do
+ActiveRecord::Schema[7.2].define(version: 2024_02_03_030754) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -56,7 +56,6 @@ ActiveRecord::Schema[7.2].define(version: 2024_02_02_230325) do
   end
 
   create_table "accounts", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
-    t.string "type"
     t.string "subtype"
     t.uuid "family_id", null: false
     t.string "name"
@@ -68,7 +67,6 @@ ActiveRecord::Schema[7.2].define(version: 2024_02_02_230325) do
     t.uuid "accountable_id"
     t.index ["accountable_type"], name: "index_accounts_on_accountable_type"
     t.index ["family_id"], name: "index_accounts_on_family_id"
-    t.index ["type"], name: "index_accounts_on_type"
   end
 
   create_table "families", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
