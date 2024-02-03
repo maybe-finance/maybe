69698d0463bfdf71a08d2475443382bb1f538588
Lee McAlilly <leemc@hey.com>
2024-02-03 13:07:23 -0600

    Add initial model tests with starter fixtures for User, Family, and Account (#281)
    * Add initial user fixture
    
    * Add initial tests for  the User model
    
    * Add initial tests for Family and a valid Account
    
    * Pass rubocop

--- test/models/account_test.rb
+++ test/models/account_test.rb
@@ -1,7 +1,11 @@
 require "test_helper"
 
 class AccountTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "new account should be valid" do
+    depository = Account::Depository.create!
+    account = Account.create!(family: families(:dylan_family), name: "Explicit Checking", balance: 1200, accountable: depository)
+    assert account.valid?
+    assert_not_nil account.accountable_id
+    assert_not_nil account.accountable
+  end
 end
