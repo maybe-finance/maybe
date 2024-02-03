cdf662557f6190c8ad1d76e4b86f84c4c4126ce2
Jose Farias <jose@farias.mx>
2024-02-03 00:12:59 -0600

    Revert "Remove safe navigation"
    This reverts commit 56b5e01e5e0ab9f54a9a5d9f5559e29897d239a4.

--- app/helpers/auth_messages_helper.rb
+++ app/helpers/auth_messages_helper.rb
@@ -1,5 +1,6 @@
 module AuthMessagesHelper
-  def auth_messages(form)
-    render "shared/auth_messages", flash: flash, errors: form.object.errors.full_messages
+  def auth_messages(form = nil)
+    render "shared/auth_messages", flash: flash,
+      errors: form&.object&.errors&.full_messages || []
   end
 end
