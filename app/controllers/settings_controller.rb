aed9b3b458b5844309e1ccf4af7baca29c547838
Edrick Leong <10529706+edrickleong@users.noreply.github.com>
2024-02-04 00:39:34 +0800

    fix: formatting

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -13,6 +13,7 @@ class SettingsController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, family_attributes: [:name, :id])
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation,
+                                 family_attributes: [ :name, :id ])
   end
 end
