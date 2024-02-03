38ac8bc107c941a293b56138b869a821fcd9e5c3
Josh Pigford <josh@joshpigford.com>
2024-02-03 17:10:04 -0600

    Fixed linter errors

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,13 +69,13 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address:              ENV['SMTP_ADDRESS'],
-    port:                 ENV['SMTP_PORT'],
-    domain:               ENV['SMTP_DOMAIN'],
-    user_name:            ENV['SMTP_USERNAME'],
-    password:             ENV['SMTP_PASSWORD'],
-    authentication:       ENV['SMTP_AUTHENTICATION'],
-    enable_starttls_auto: ENV['SMTP_ENABLE_STARTTLS_AUTO']
+    address:              ENV["SMTP_ADDRESS"],
+    port:                 ENV["SMTP_PORT"],
+    domain:               ENV["SMTP_DOMAIN"],
+    user_name:            ENV["SMTP_USERNAME"],
+    password:             ENV["SMTP_PASSWORD"],
+    authentication:       ENV["SMTP_AUTHENTICATION"],
+    enable_starttls_auto: ENV["SMTP_ENABLE_STARTTLS_AUTO"]
   }
 
   # Ignore bad email addresses and do not raise email delivery errors.
