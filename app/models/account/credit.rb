a2b96db75df547b8f050d21bc3636214eb53548b
Rob Zolkos <rob@zolkos.com>
2024-02-03 15:38:52 -0500

    Remove styling configuration for account types out (#284)
    * Remve styling configuration for account types out
    
    * No need to tailwind config for models

--- app/models/account/credit.rb
+++ app/models/account/credit.rb
@@ -1,18 +1,7 @@
 class Account::Credit < ApplicationRecord
   include Accountable
 
-  def icon
-    "icon-credit-card.svg"
-  end
-
   def type_name
     "Credit Card"
-  end
-
-  def color
-    {
-      background: "bg-[#E6F6FA]",
-      text: "text-[#189FC7]"
-    }
   end
 end
--- app/models/account/depository.rb
+++ app/models/account/depository.rb
@@ -1,18 +1,7 @@
 class Account::Depository < ApplicationRecord
   include Accountable
 
-  def icon
-    "icon-bank-accounts.svg"
-  end
-
   def type_name
     "Bank Accounts"
-  end
-
-  def color
-    {
-      background: "bg-[#EAF4FF]",
-      text: "text-[#3492FB]"
-    }
   end
 end
