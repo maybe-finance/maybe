b587dd19b65dce6406c54ca7ab9a0ee0b12cb445
Josh Pigford <josh@joshpigford.com>
2024-02-03 17:44:08 -0600

    App domain for mailers

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,6 +67,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.default_url_options = { host: ENV["APP_DOMAIN"] }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              ENV["SMTP_ADDRESS"],
