918d19635b29aeff5544b796888299f373be8563
Josh Pigford <josh@joshpigford.com>
2024-02-03 17:52:30 -0600

    Convert to boolean

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,7 +76,7 @@ Rails.application.configure do
     user_name:            ENV["SMTP_USERNAME"],
     password:             ENV["SMTP_PASSWORD"],
     authentication:       ENV["SMTP_AUTHENTICATION"],
-    enable_starttls_auto: ENV["SMTP_ENABLE_STARTTLS_AUTO"]
+    enable_starttls_auto: ENV["SMTP_ENABLE_STARTTLS_AUTO"] == "true"
   }
 
   # Ignore bad email addresses and do not raise email delivery errors.
