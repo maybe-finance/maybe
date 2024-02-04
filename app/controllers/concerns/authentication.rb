affdd934f74358683bbc4effdda98fdaadc3cb4b
Dwight Watson <dwightwatson@users.noreply.github.com>
2024-02-05 08:00:08 +1100

    Opt-in to Current fully

--- app/controllers/concerns/authentication.rb
+++ app/controllers/concerns/authentication.rb
@@ -2,31 +2,21 @@ module Authentication
   extend ActiveSupport::Concern
 
   included do
-    helper_method :current_user
-    helper_method :current_family
     helper_method :user_signed_in?
   end
 
   private
 
   def authenticate_user!
-    redirect_to new_session_path unless user_signed_in?
-  end
-
-  def current_user
-    Current.user || authenticate_user_from_session
-  end
-
-  def current_family
-    current_user.family
-  end
-
-  def authenticate_user_from_session
-    User.find_by(id: session[:user_id])
+    if user = User.find_by(id: session[:user_id])
+      Current.user = user
+    else
+      redirect_to new_session_url
+    end
   end
 
   def user_signed_in?
-    current_user.present?
+    Current.user.present?
   end
 
   def login(user)
