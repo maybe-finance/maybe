7c4ebc6d1a85cd6d6377cf6e91987014fdd0139c
Dwight Watson <dwightwatson@users.noreply.github.com>
2024-02-05 07:53:05 +1100

    Move sign_in to setup block

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -1,8 +1,11 @@
 require "test_helper"
 
 class PagesControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @user = sign_in users(:bob)
+  end
+
   test "dashboard" do
-    sign_in users(:bob)
     get root_path
     assert_response :ok
   end
