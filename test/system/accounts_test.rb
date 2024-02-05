e3db1e8348d519b03e7b5842e7b4e76c1458b961
Dwight Watson <dwightwatson@users.noreply.github.com>
2024-02-05 20:08:09 +1100

    First pass at a system test

---
+++ test/system/accounts_test.rb
@@ -0,0 +1,18 @@
+require "application_system_test_case"
+
+class AccountsTest < ApplicationSystemTestCase
+  setup do
+    sign_in @user = users(:bob)
+  end
+
+  test "should create account" do
+    click_on "New account"
+    click_on "Credit Card"
+    within "form" do
+      fill_in "Name", with: "VISA"
+      fill_in "Balance", with: "1000"
+      click_on "Submit"
+    end
+    assert_text "$1,000.00"
+  end
+end
