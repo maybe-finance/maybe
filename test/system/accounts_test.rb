5de519945901f07a4ea39a6a7f7ba6994f8c9ae3
Pedro López Mareque <Pedro.lopez.mareque@gmail.com>
2024-02-07 16:09:35 +0100

    Skip system test instead of comment it (#348)
    * feat: add cursor pointer in the log-in and create account pages, also make full with (it's cutted right now) the fileds in the settings edit page
    
    * feat: skip system test with an explanation instead of comment them
    
    * fix typo in the skip

--- test/system/accounts_test.rb
+++ test/system/accounts_test.rb
@@ -5,14 +5,16 @@ class AccountsTest < ApplicationSystemTestCase
     sign_in @user = users(:bob)
   end
 
-  # test "should create account" do
-  #   click_on "New account"
-  #   click_on "Credit Card"
-  #   within "form" do
-  #     fill_in "Name", with: "VISA"
-  #     fill_in "Balance", with: "1000"
-  #     click_on "Submit"
-  #   end
-  #   assert_text "$1,000"
-  # end
+  test "should create account" do
+    skip("Disabling this test for now, UI is changing to quickly to do systems testing")
+
+    click_on "New account"
+    click_on "Credit Card"
+    within "form" do
+      fill_in "Name", with: "VISA"
+      fill_in "Balance", with: "1000"
+      click_on "Submit"
+    end
+    assert_text "$1,000"
+  end
 end
