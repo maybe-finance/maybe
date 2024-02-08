45ba88add605308f28c9e84dd2966d2407631b09
eddbot <edwardsansome@gmail.com>
2024-02-08 21:50:11 +0000

    use view helper

--- app/helpers/accounts_helper.rb
+++ app/helpers/accounts_helper.rb
@@ -2,4 +2,16 @@ module AccountsHelper
   def to_accountable_title(accountable)
     accountable.model_name.human
   end
+
+  def humanized_account
+    account_type.model_name.human
+  end
+
+  def account_type
+    type = params[:type]
+
+    Account unless type.present? && Account.accountable_types.include?(params[:type])
+
+    "Account::#{type}".constantize
+  end
 end
