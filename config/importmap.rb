4761619870d88d4366c8b5a213217a720a7b2473
Josh Pigford <josh@joshpigford.com>
2024-02-08 10:46:05 -0600

    New Add Account UI
    * Add Lucide gem (#364)
    
    * feat: add cursor pointer in the log-in and create account pages, also make full with (it's cutted right now) the fileds in the settings edit page
    
    * feat: skip system test with an explanation instead of comment them
    
    * fix typo in the skip
    
    * feat: add lucide gem
    
    * Add reusable modal (#362)
    
    * Remove unused form
    
    * Add reusable modal
    
    * Prelim styling
    
    * Add instructions
    
    ---------
    
    Co-authored-by: Josh Pigford <josh@joshpigford.com>
    
    * Add keyboard navigation to new account selector (#375)
    
    * New account menu (#372)
    
    * New account menu
    
    * Styling tweaks
    
    ---------
    
    Signed-off-by: Josh Pigford <josh@joshpigford.com>
    
    * Entry method links (#376)
    
    * Initial add account form (#378)
    
    * Initial add account form
    
    * Unused
    
    ---------
    
    Signed-off-by: Josh Pigford <josh@joshpigford.com>
    Co-authored-by: Pedro López Mareque <Pedro.lopez.mareque@gmail.com>
    Co-authored-by: Rob Zolkos <rob@zolkos.com>
    Co-authored-by: Josh Brown <josh@joossh.com>

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,3 +5,4 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "@github/hotkey", to: "@github--hotkey.js" # @3.1.0
