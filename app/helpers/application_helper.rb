4e6835358ddfd92acfc3ac6565f535375808290e
Josh Brown <josh@joossh.com>
2024-02-09 11:57:01 +0000

    Fix modal translation scope
    By changing this to a partial that accepts the content via a local var
    instead of a layout the virtual path, and therefore translation scope is
    not changed.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,14 +13,8 @@ module ApplicationHelper
 
   # Wrap view with <%= modal do %> ... <% end %> to have it open in a modal
   # Make sure to add data-turbo-frame="modal" to the link/button that opens the modal
-  def modal
-    # Save the current virtual path so we can restore it after rendering the modal
-    # This ensures translations are scoped to the current view appropriately
-    path = @virtual_path
-
-    render "shared/modal" do
-      @virtual_path = path
-      yield
-    end
+  def modal(&block)
+    content = capture &block
+    render partial: "shared/modal", locals: { content: content }
   end
 end
