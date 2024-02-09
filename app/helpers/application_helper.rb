af467d104d95abf839a2a1ecf2b51f883215cf72
Josh Brown <josh@joossh.com>
2024-02-09 10:21:42 +0000

    Patch virtual_path within modal helper
    Added an example translation within the modal to test.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,7 +13,14 @@ module ApplicationHelper
 
   # Wrap view with <%= modal do %> ... <% end %> to have it open in a modal
   # Make sure to add data-turbo-frame="modal" to the link/button that opens the modal
-  def modal(&block)
-    render "shared/modal", &block
+  def modal
+    # Save the current virtual path so we can restore it after rendering the modal
+    # This ensures translations are scoped to the current view appropriately
+    path = @virtual_path
+
+    render "shared/modal" do
+      @virtual_path = path
+      yield
+    end
   end
 end
