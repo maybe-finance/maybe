5c8256862dd874b365fab7ff978a8dd6ed87e239
Cristino Crolla <cristianocrolla@gmail.com>
2024-02-10 15:14:50 +0000

    revert form_field_tag changes

--- app/helpers/forms_helper.rb
+++ app/helpers/forms_helper.rb
@@ -1,6 +1,5 @@
 module FormsHelper
-  def form_field_tag(&block)
-    content = capture(&block)
-    tag.div content, class: "form-field"
+  def form_field_tag(&)
+    tag.div class: "form-field", &
   end
 end
