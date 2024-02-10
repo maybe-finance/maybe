8906fa81d8540dd676aa92fa1598e907cdc8292c
Cristino Crolla <cristianocrolla@gmail.com>
2024-02-10 14:35:27 +0000

    remove block context from form builder in favour of using form tag directly

--- app/helpers/application_form_builder.rb
+++ app/helpers/application_form_builder.rb
@@ -8,17 +8,15 @@ class ApplicationFormBuilder < ActionView::Helpers::FormBuilder
 
   (field_helpers - [ :label, :check_box, :radio_button, :fields_for, :fields, :hidden_field, :file_field ]).each do |selector|
     class_eval <<-RUBY_EVAL, __FILE__, __LINE__ + 1
-      def #{selector}(method, options, &block)
+      def #{selector}(method, options)
         default_options = { class: "form-field__input" }
         merged_options = default_options.merge(options)
-        content = block_given? ? @template.capture(&block) : ''
 
         return super(method, merged_options) unless options[:label]
 
         @template.form_field_tag do
           label(method, *label_args(options)) +
-          super(method, merged_options.except(:label)) +
-          content
+          super(method, merged_options.except(:label))
         end
       end
     RUBY_EVAL
--- app/helpers/forms_helper.rb
+++ app/helpers/forms_helper.rb
@@ -1,5 +1,6 @@
 module FormsHelper
-  def form_field_tag(&)
-    tag.div class: "form-field", &
+  def form_field_tag(&block)
+    content = capture(&block)
+    tag.div content, class: "form-field"
   end
 end
