2857b91043969479dbb0d78b779d8504a5ce3624
sergey <sergey.tyan.iitu@gmail.com>
2024-02-11 14:50:40 +0800

    Fix indentation

--- app/javascript/controllers/account_collapse_controller.js
+++ app/javascript/controllers/account_collapse_controller.js
@@ -2,54 +2,52 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="account-collapse"
 export default class extends Controller {
+  static values = { type: String }
+  boundOnToggle = null
+  initialToggle = false
+  STORAGE_NAME = "accountCollapseStates"
 
-	static values = { type: String }
-	boundOnToggle = null
-	initialToggle = false
-	STORAGE_NAME = 'accountCollapseStates'
-
-	connect() {
-		this.boundOnToggle = this.onToggle.bind(this)
-    this.element
-      .addEventListener("toggle", this.boundOnToggle)
-		this.updateFromLocalStorage()
+  connect() {
+    this.boundOnToggle = this.onToggle.bind(this)
+    this.element.addEventListener("toggle", this.boundOnToggle)
+    this.updateFromLocalStorage()
   }
 
   disconnect() {
-		this.element.removeEventListener("toggle", this.boundOnToggle)
-	}
+    this.element.removeEventListener("toggle", this.boundOnToggle)
+  }
 
-	onToggle() {
-		if (this.initialToggle) {
+  onToggle() {
+    if (this.initialToggle) {
       this.initialToggle = false
       return
     }
-		
-		const items = this.getItemsFromLocalStorage()
+
+    const items = this.getItemsFromLocalStorage()
     if (items.has(this.typeValue)) {
-			items.delete(this.typeValue)
-		} else {
-			items.add(this.typeValue)
-		}
-		localStorage.setItem(this.STORAGE_NAME, JSON.stringify([...items]))
-	}
+      items.delete(this.typeValue)
+    } else {
+      items.add(this.typeValue)
+    }
+    localStorage.setItem(this.STORAGE_NAME, JSON.stringify([...items]))
+  }
 
   updateFromLocalStorage() {
     const items = this.getItemsFromLocalStorage()
-    
+
     if (items.has(this.typeValue)) {
-			this.initialToggle = true
-			this.element.setAttribute('open', '')
+      this.initialToggle = true
+      this.element.setAttribute("open", "")
     }
   }
 
   getItemsFromLocalStorage() {
     try {
-			const items = localStorage.getItem(this.STORAGE_NAME)
-			return new Set(items ? JSON.parse(items) : [])
-		} catch (error) {
-			console.error("Error parsing items from localStorage:", error)
-			return new Set()
-		}
+      const items = localStorage.getItem(this.STORAGE_NAME)
+      return new Set(items ? JSON.parse(items) : [])
+    } catch (error) {
+      console.error("Error parsing items from localStorage:", error)
+      return new Set()
+    }
   }
 }
