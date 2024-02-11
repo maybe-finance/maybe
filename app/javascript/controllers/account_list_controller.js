6d30b3e1a6e1404a76d6d935bcb8106f000e4c4c
Ashley Pean <pean.ashley@gmail.com>
2024-02-11 03:44:21 -0500

    cleanup

--- app/javascript/controllers/account_list_controller.js
+++ app/javascript/controllers/account_list_controller.js
@@ -2,9 +2,11 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="account-list"
 export default class extends Controller {
+  static values = { category: String }
+
   getItem(key) {
     try {
-      return JSON.parse(localStorage.getItem(key))
+      return JSON.parse(localStorage.getItem(key)) ?? {}
     } catch(err) {
       console.err('Error retrieving from localstorage', err)
       return null
@@ -20,24 +22,20 @@ export default class extends Controller {
   }
   
   handleToggle() {
-    const { accountType } = this.element.dataset
     let config = this.getItem('accountList')
 
-    if (!config) config = {}
-
-    config[accountType] = this.element.open
+    config[this.categoryValue] = this.element.open
     this.setItem('accountList', config)
   }
 
   connect() {
-    const { accountType } = this.element.dataset
-    let config = this.getItem('accountList') || {}
+    let config = this.getItem('accountList')
     
-    if (!config[accountType]) {
-      config[accountType] = this.element.open
+    if (!config.hasOwnProperty(this.categoryValue)) {
+      config[this.categoryValue] = this.element.open
       this.setItem('accountList', config)
     } 
     
-    this.element.open = config[accountType]
+    this.element.open = config[this.categoryValue]
   }
 }
