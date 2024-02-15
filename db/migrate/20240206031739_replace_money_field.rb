093ddba28f430f7954e97323f1c7be13d1362023
Adrien Poly <adrienpoly@gmail.com>
2024-02-15 01:48:35 +0100

    Update database migrations and strengthen db smoke tests (#462)

--- db/migrate/20240206031739_replace_money_field.rb
+++ db/migrate/20240206031739_replace_money_field.rb
@@ -1,6 +1,6 @@
 class ReplaceMoneyField < ActiveRecord::Migration[7.2]
   def change
-    add_column :accounts, :balance_cents
+    add_column :accounts, :balance_cents, :integer
     change_column :accounts, :balance_cents, :integer, limit: 8
 
     Account.reset_column_information
