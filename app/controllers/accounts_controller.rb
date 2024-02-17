d5361a01443ed4a90331e3d34430b510e048ec8b
Zach Gollwitzer <zach.gollwitzer@gmail.com>
2024-02-17 09:05:26 -0500

    Remove test data

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -9,9 +9,7 @@ class AccountsController < ApplicationController
   end
 
   def show
-    # Temporary while dummy data is being used
     @account = Current.family.accounts.find(params[:id])
-    @test_account = sample_account
   end
 
   def create
@@ -29,34 +27,5 @@ class AccountsController < ApplicationController
 
   def account_params
     params.require(:account).permit(:name, :accountable_type, :original_balance, :original_currency, :subtype)
-  end
-
-  def sample_account
-    OpenStruct.new(
-      id: 1,
-      name: "Sample Account",
-      original_balance: BigDecimal("1115181"),
-      original_currency: "USD",
-      converted_balance: BigDecimal("1115181"), # Assuming conversion rate is 1 for simplicity
-      converted_currency: "USD",
-      dollar_change: BigDecimal("1553.43"), # Added dollar change
-      percent_change: BigDecimal("0.9"), # Added percent change
-      subtype: "Checking",
-      accountable_type: "Depository",
-      balances: sample_balances
-    )
-  end
-
-  def sample_balances
-    4.times.map do |i|
-      OpenStruct.new(
-        date: "Feb #{12 + i} 2024",
-        description: "Manually entered",
-        amount: BigDecimal("1000") + (i * BigDecimal("100")),
-        change: i == 3 ? -50 : (i == 2 ? 0 : 100 + (i * 10)),
-        percentage_change: i == 3 ? -5 : (i == 2 ? 0 : 10 + i),
-        icon: i == 3 ? "arrow-down" : (i == 2 ? "minus" : (i.even? ? "arrow-down" : "arrow-up"))
-      )
-    end
   end
 end
