9484f32103dc797288474d1e75e5d81fb1fea344
Jean Moreau <jean.moreau@gmail.com>
2024-03-08 12:36:08 -0500

    sort transactions by date so grouping works appropriately with pagination

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -3,7 +3,7 @@ class TransactionsController < ApplicationController
   before_action :set_transaction, only: %i[ show edit update destroy ]
 
   def index
-    @pagy, @transactions = pagy(Current.family.transactions, items: 50)
+    @pagy, @transactions = pagy(Current.family.transactions.order(date: :desc), items: 50)
   end
 
   def show
