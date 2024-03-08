e59c1acf3c0d330bae82e72f48f2f0fefa795617
Jean Moreau <jean.moreau@gmail.com>
2024-03-08 12:42:06 -0500

    add space between mobile view buttons

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -3,7 +3,7 @@ class TransactionsController < ApplicationController
   before_action :set_transaction, only: %i[ show edit update destroy ]
 
   def index
-    @pagy, @transactions = pagy(Current.family.transactions.order(date: :desc), items: 50)
+    @pagy, @transactions = pagy(Current.family.transactions.order(date: :desc), items: 4)
   end
 
   def show
