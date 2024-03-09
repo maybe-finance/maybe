0d126891e401aab67bcaf5c1d77a1204d6ab288d
Zach Gollwitzer <zach.gollwitzer@gmail.com>
2024-03-09 12:01:36 -0500

    Remove currencies from seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,4 @@
 # development, test). The code here should be idempotent so that it can be executed at any point in every environment.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 
-# Create default currency
-Currency.find_or_create_by(iso_code: "USD", name: "United States Dollar")
-
 puts 'Run the following command to create demo data: `rake demo_data:reset`' if Rails.env.development?
