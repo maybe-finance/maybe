9706b3f61a079f5145c8012a207d66f03861d2a3
Adrien POLY <adrienpoly@gmail.com>
2024-03-25 09:44:57 +0100

    add uniq index for users email to ensure uniqueness

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_03_19_154732) do
+ActiveRecord::Schema[7.2].define(version: 2024_03_25_064211) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -232,6 +232,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_03_19_154732) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["family_id"], name: "index_users_on_family_id"
   end
 
