16b5d51d58874e73affced2bd46ae37e39da199d
Cristino Crolla <cristianocrolla@gmail.com>
2024-03-28 23:18:40 +0000

    Add default fallback check

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -8,13 +8,22 @@ window.Stimulus   = application
 
 Turbo.setConfirmMethod((message, element) => {
   let dialog = document.getElementById("turbo-confirm")
-  let msg = JSON.parse(message)
+  let msg;
 
-  dialog.querySelector("[id='turbo-confirm-title']").innerHTML = msg.title;
-  dialog.querySelector("[id='turbo-confirm-body']").innerHTML = msg.body;
-  dialog.querySelector("[id='turbo-confirm-accept']").innerHTML = msg.accept;
+  try {
+    msg = JSON.parse(message)
+  } catch (e) {
+    msg = {
+      title: "Are you sure?",
+      body: "You will not be able to undo this decision",
+      accept: "Confirm"
+    }
+  }
 
-  // prevent background from scrolling
+  dialog.querySelector("[id='turbo-confirm-title']").innerHTML = msg.title ? msg.title : "Are you sure?";
+  dialog.querySelector("[id='turbo-confirm-body']").innerHTML = msg.body ? msg.body : "You will not be able to undo this decision";
+  dialog.querySelector("[id='turbo-confirm-accept']").innerHTML = msg.accept ? msg.accept : "Confirm";
+
   dialog.showModal()
 
   return new Promise((resolve, reject) => {
