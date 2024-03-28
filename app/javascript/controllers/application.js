21e3bd0849a9d673f184ad5ce050f485986ef284
Cristino Crolla <cristianocrolla@gmail.com>
2024-03-28 22:35:32 +0000

    Add turbo confirm dialog

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -6,4 +6,22 @@ const application = Application.start()
 application.debug = false
 window.Stimulus   = application
 
+Turbo.setConfirmMethod((message, element) => {
+  let dialog = document.getElementById("turbo-confirm")
+  let msg = JSON.parse(message)
+
+  dialog.querySelector("[id='turbo-confirm-title']").innerHTML = msg.title;
+  dialog.querySelector("[id='turbo-confirm-body']").innerHTML = msg.body;
+  dialog.querySelector("[id='turbo-confirm-accept']").innerHTML = msg.accept;
+
+  // prevent background from scrolling
+  dialog.showModal()
+
+  return new Promise((resolve, reject) => {
+    dialog.addEventListener("close", () => {
+        resolve(dialog.returnValue == "confirm")
+    }, { once: true })
+  })
+})
+
 export { application }
