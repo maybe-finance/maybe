cf06e0e7a6ce69e394dffd55a43eb46afe99e20a
Mattia Malnis <malnis.mattia@gmail.com>
2024-03-30 08:50:50 +0100

    Revert "Fix ActiveJob::DeserializationError in AccountSyncJob when an account doesn't exists"
    This reverts commit 8dbf634819d08b9d55679f9e137e901608070ba0.

--- app/jobs/account_sync_job.rb
+++ app/jobs/account_sync_job.rb
@@ -1,10 +1,7 @@
 class AccountSyncJob < ApplicationJob
   queue_as :default
 
-  def perform(account_id)
-    account = Account.find_by(id: account_id)
-    return unless account.present?
-
+  def perform(account)
     account.sync
   end
 end
