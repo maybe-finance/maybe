b3c48d13e70780caa146cdb79fce6bb851fabd76
Thibaut Gorioux <gorioux.thibaut@gmail.com>
2024-04-02 18:17:26 +0200

    Filter transactions by category (#581)
    * Add transaction filtering by category
    
    * Link label to checkbox
    
    * Keep the dropdown open when clicked to allow tab change
    
    * Show the badge with color and name when filter is applied
    
    * Reduce color height

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -32,6 +32,10 @@ class Transaction < ApplicationRecord
           value.each do |account_id|
             filters << { type: "account", value: family.accounts.find(account_id), original: { key: key, value: account_id } }
           end
+        when "category_id_in"
+          value.each do |category_id|
+            filters << { type: "category", value: family.transaction_categories.find(category_id), original: { key: key, value: category_id } }
+          end
         when "category_name_or_account_name_or_name_cont"
           filters << { type: "search", value: value, original: { key: key, value: nil } }
         end
