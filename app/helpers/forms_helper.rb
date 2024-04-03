4f0b2de4ef6a9547f190e103d8d49f44ca368708
Zach Gollwitzer <zach.gollwitzer@gmail.com>
2024-04-03 17:32:27 -0400

    Consolidate dropdown controllers (#600)
    * Basic listbox and popover controllers with temporary example
    
    * Separate select and menu controllers

--- app/helpers/forms_helper.rb
+++ app/helpers/forms_helper.rb
@@ -2,8 +2,4 @@ module FormsHelper
   def form_field_tag(&)
     tag.div class: "form-field", &
   end
-
-  def currency_dropdown(f: nil, options: [])
-    render partial: "shared/currency_dropdown", locals: { f: f, options: options }
-  end
 end
