e175aa9b491caf64ec847d95bfcb40096cef39c9
Jose Farias <jose@farias.mx>
2024-04-18 18:03:52 -0600

    Reindent TimeSeries classes

--- app/models/time_series/value.rb
+++ app/models/time_series/value.rb
@@ -1,32 +1,32 @@
 class TimeSeries::Value
-    include Comparable
+  include Comparable
 
-    attr_accessor :trend
-    attr_reader :value, :date, :original
+  attr_accessor :trend
+  attr_reader :value, :date, :original
 
-    def initialize(obj)
-        @original = obj.fetch(:original, obj)
+  def initialize(obj)
+    @original = obj.fetch(:original, obj)
 
-        if obj.is_a?(Hash)
-            @date = obj[:date]
-            @value = obj[:value]
-        else
-            @date = obj.date
-            @value = obj.value
-        end
-
-        validate_input
+    if obj.is_a?(Hash)
+      @date = obj[:date]
+      @value = obj[:value]
+    else
+      @date = obj.date
+      @value = obj.value
     end
 
-    def <=>(other)
-        result = date <=> other.date
-        result = value <=> other.value if result == 0
-        result
-    end
+    validate_input
+  end
 
-    private
-        def validate_input
-            raise ArgumentError, "Date is required" unless @date.is_a?(Date)
-            raise ArgumentError, "Money or Numeric value is required" unless @value.is_a?(Money) || @value.is_a?(Numeric)
-        end
+  def <=>(other)
+    result = date <=> other.date
+    result = value <=> other.value if result == 0
+    result
+  end
+
+  private
+    def validate_input
+      raise ArgumentError, "Date is required" unless @date.is_a?(Date)
+      raise ArgumentError, "Money or Numeric value is required" unless @value.is_a?(Money) || @value.is_a?(Numeric)
+    end
 end
