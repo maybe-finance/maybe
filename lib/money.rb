31caf7056d7a0c81934f437ad716a4558fb3890d
Jose Farias <jose@farias.mx>
2024-04-18 21:06:47 -0600

    Refactor Money

--- lib/money.rb
+++ lib/money.rb
@@ -1,8 +1,10 @@
 class Money
-  include Comparable
-  include Arithmetic
+  include Comparable, Arithmetic
+  include ActiveModel::Validations
 
   attr_reader :amount, :currency
+
+  validate :source_must_be_of_known_type
 
   class << self
     def default_currency
@@ -15,59 +17,78 @@ class Money
   end
 
   def initialize(obj, currency = Money.default_currency)
-    unless obj.is_a?(Money) || obj.is_a?(Numeric) || obj.is_a?(BigDecimal)
-        raise ArgumentError, "obj must be an instance of Money, Numeric, or BigDecimal"
-    end
-
+    @source = obj
     @amount = obj.is_a?(Money) ? obj.amount : BigDecimal(obj.to_s)
     @currency = obj.is_a?(Money) ? obj.currency : Money::Currency.new(currency)
+
+    validate!
   end
 
   # TODO: Replace with injected rate store
   def exchange_to(other_currency, date = Date.current)
-    return self if @currency == Money::Currency.new(other_currency)
-    rate = ExchangeRate.find_rate(from: @currency, to: other_currency, date: date)
-    return nil if rate.nil?
-    Money.new(@amount * rate.rate, other_currency)
+    if currency == Money::Currency.new(other_currency)
+      self
+    elsif rate = ExchangeRate.find_rate(from: currency, to: other_currency, date: date)
+      Money.new(amount * rate.rate, other_currency)
+    end
   end
 
-  def cents_str(precision = @currency.default_precision)
+  def cents_str(precision = currency.default_precision)
     format_str = "%.#{precision}f"
-    amount_str = format_str % @amount
-    parts = amount_str.split(@currency.separator)
+    amount_str = format_str % amount
+    parts = amount_str.split(currency.separator)
 
-    return "" if parts.length < 2
-
-    parts.last.ljust(precision, "0")
+    if parts.length < 2
+      ""
+    else
+      parts.last.ljust(precision, "0")
+    end
   end
 
-  # Basic formatting only.  Use the Rails number_to_currency helper for more advanced formatting.
-  alias to_s format
+  # Use `format` for basic formatting only.
+  # Use the Rails number_to_currency helper for more advanced formatting.
   def format
-    whole_part, fractional_part = sprintf("%.#{@currency.default_precision}f", @amount).split(".")
-    whole_with_delimiters = whole_part.chars.to_a.reverse.each_slice(3).map(&:join).join(@currency.delimiter).reverse
-    formatted_amount = "#{whole_with_delimiters}#{@currency.separator}#{fractional_part}"
-    @currency.default_format.gsub("%n", formatted_amount).gsub("%u", @currency.symbol)
+    whole_part, fractional_part = sprintf("%.#{currency.default_precision}f", amount).split(".")
+    whole_with_delimiters = whole_part.chars.to_a.reverse.each_slice(3).map(&:join).join(currency.delimiter).reverse
+    formatted_amount = "#{whole_with_delimiters}#{currency.separator}#{fractional_part}"
+
+    currency.default_format.gsub("%n", formatted_amount).gsub("%u", currency.symbol)
   end
+  alias_method :to_s, :format
 
   def as_json
-    { amount: @amount, currency: @currency.iso_code }.as_json
+    { amount: amount, currency: currency.iso_code }.as_json
   end
 
   def <=>(other)
     raise TypeError, "Money can only be compared with other Money objects except for 0" unless other.is_a?(Money) || other.eql?(0)
-    return @amount <=> other if other.is_a?(Numeric)
-    amount_comparison = @amount <=> other.amount
-    return amount_comparison unless amount_comparison == 0
-    @currency <=> other.currency
+
+    if other.is_a?(Numeric)
+      amount <=> other
+    else
+      amount_comparison = amount <=> other.amount
+
+      if amount_comparison == 0
+        currency <=> other.currency
+      else
+        amount_comparison
+      end
+    end
   end
 
   def default_format_options
     {
-      unit: @currency.symbol,
-      precision: @currency.default_precision,
-      delimiter: @currency.delimiter,
-      separator: @currency.separator
+      unit: currency.symbol,
+      precision: currency.default_precision,
+      delimiter: currency.delimiter,
+      separator: currency.separator
     }
   end
+
+  private
+    def source_must_be_of_known_type
+      unless @source.is_a?(Money) || @source.is_a?(Numeric) || @source.is_a?(BigDecimal)
+        errors.add :source, "must be a Money, Numeric, or BigDecimal"
+      end
+    end
 end
