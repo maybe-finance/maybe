f4cb650ec4a3009d081076e4c4d9cd3bf735ce07
TiboGrx <gorioux.thibaut@gmail.com>
2024-04-19 11:44:20 +0200

    Add test with auth

--- test/controllers/currencies_controller_test.rb
+++ test/controllers/currencies_controller_test.rb
@@ -1,8 +1,12 @@
 require "test_helper"
 
 class CurrenciesControllerTest < ActionDispatch::IntegrationTest
-  test "should get show" do
-    get currencies_show_url
+  setup do
+    sign_in @user = users(:family_admin)
+  end
+
+  test "should show currency" do
+    get currency_url(id: "EUR", format: :json)
     assert_response :success
   end
 end
