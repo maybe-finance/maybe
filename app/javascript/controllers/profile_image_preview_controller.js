1d50b23629b8b8e6d6cb14fa4414735817a1cbed
Christian Robinson <christian@robbo.dev>
2024-04-27 09:20:58 +0100

    Update markup/stimulus controller

--- app/javascript/controllers/profile_image_preview_controller.js
+++ app/javascript/controllers/profile_image_preview_controller.js
@@ -1,11 +1,7 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["imagePreview", "fileField", "clearButton"]
-
-  initialize() {
-    this.orgiginalContent = this.imagePreviewTarget.innerHTML;
-  }
+  static targets = ["imagePreview", "fileField", "clearBtn", "template"]
 
   preview(event) {
     const file = event.target.files[0];
@@ -13,7 +9,8 @@ export default class extends Controller {
       const reader = new FileReader();
       reader.onload = (e) => {
         this.imagePreviewTarget.innerHTML = `<img src="${e.target.result}" alt="Preview" class="w-24 h-24 rounded-full object-cover" />`;
-        this.clearButtonTarget.classList.remove("hidden");
+        this.templateTarget.classList.add("hidden");
+        this.clearBtnTarget.classList.remove("hidden");
       };
       reader.readAsDataURL(file);
     }
@@ -21,7 +18,8 @@ export default class extends Controller {
 
   clear() {
     this.fileFieldTarget.value = null;
-    this.imagePreviewTarget.innerHTML = this.orgiginalContent;
-    this.clearButtonTarget.classList.add("hidden");
+    this.templateTarget.classList.remove("hidden");
+    this.imagePreviewTarget.innerHTML = this.templateTarget.innerHTML;
+    this.clearBtnTarget.classList.add("hidden");
   }
 }
