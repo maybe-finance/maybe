86b283316a41f1460b9081e13a0f1b61801eb474
TiboGrx <gorioux.thibaut@gmail.com>
2024-04-27 23:40:31 +0200

    Rework application mailer

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,23 +1,23 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: Setting.email_sender
+  default from: ENV["EMAIL_SENDER"]
   layout "mailer"
 
-  def mail(headers = {}, &block)
-    if Rails.application.config.action_mailer.delivery_method = :smtp
-      headers[:delivery_method_options] = smtp_settings
-    end
-
-    super(headers, &block)
-  end
+  before_action :set_self_host_settings, if: -> { ENV["SELF_HOSTING_ENABLED"] == "true" }
 
   private
-    def smtp_settings
-      {
-        address:   Setting.smtp_host,
-        port:      Setting.smtp_port,
-        user_name: Setting.smtp_username,
-        password:  Setting.smtp_password,
-        tls:       ENV["SMTP_TLS_ENABLED"] == "true"
-        }
+
+  def set_self_host_settings
+    ActionMailer::Base.default(from: Setting.email_sender)
+    if Rails.application.config.action_mailer.delivery_method = :smtp
+      set_self_host_smtp_settings
     end
+  end
+
+  def set_self_host_smtp_settings
+    ActionMailer::Base.smtp_settings = { address: Setting.smtp_host,
+                                         port: Setting.smtp_port,
+                                         user_name: Setting.smtp_username,
+                                         password: Setting.smtp_password,
+                                         tls: ENV["SMTP_TLS_ENABLED"] == "true" }
+  end
 end
