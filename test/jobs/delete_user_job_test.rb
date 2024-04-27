b494d0546eaa5e575d59eaa6cd9bfa00bfea4f16
Jemilu Mohammed <jemilu@crowncitytechnologies.com>
2024-04-27 12:31:31 +0000

    improve test coverage for delete_user_job and do cleanups

--- test/jobs/delete_user_job_test.rb
+++ test/jobs/delete_user_job_test.rb
@@ -8,8 +8,34 @@ class DeleteUserJobTest < ActiveJob::TestCase
   end
 
   test "can be enqueued with user attributes" do
+    assert_enqueued_with(job: DeleteUserJob, args: [@user_member.attributes]) do
+      DeleteUserJob.perform_later(@user_member.attributes)
+    end
+  end
+
+  test "can be enqueued with admin user attributes" do
     assert_enqueued_with(job: DeleteUserJob, args: [@user_admin.attributes]) do
       DeleteUserJob.perform_later(@user_admin.attributes)
     end
+  end
+
+  test "deleting a member with an admin will not delete the family" do
+    DeleteUserJob.perform_now(@user_member.attributes)
+    Family.find(@user_member.family_id)
+  end
+
+  test "family, related accounts and other members are deleted for the last remaining admin" do
+    DeleteUserJob.perform_now(@user_admin.attributes)
+
+    assert_raises(ActiveRecord::RecordNotFound) do
+      puts Family.find(@user_admin.family_id)
+    end
+
+    assert_raises(ActiveRecord::RecordNotFound) do
+      puts Family.find(@user_admin.family_id).accounts
+    end
+
+    assert_equal Account.where(family_id: @user_admin.family_id).count, 0
+    assert_equal User.where(family_id: @user_admin.family_id).count, 0
   end
 end
