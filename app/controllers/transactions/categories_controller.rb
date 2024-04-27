2d46d1198834c87b56c1380e7c5a7e475190a203
Jose Farias <jose@farias.mx>
2024-04-27 11:59:19 -0600

    Wire up transaction category creation

--- app/controllers/transactions/categories_controller.rb
+++ app/controllers/transactions/categories_controller.rb
@@ -1,12 +1,20 @@
 class Transactions::CategoriesController < ApplicationController
   before_action :set_category, only: %i[ edit update destroy ]
+  before_action :set_transaction, only: :create
 
   def index
     @categories = Current.family.transaction_categories.alphabetically
   end
 
+  def new
+    @category = Current.family.transaction_categories.new color: Transaction::Category::COLORS.sample
+  end
+
   def create
-    Current.family.transaction_categories.create! category_params
+    Transaction::Category.transaction do
+      category = Current.family.transaction_categories.create!(category_params)
+      @transaction.update!(category_id: category.id) if @transaction
+    end
 
     redirect_back_or_to transactions_path, notice: t(".success")
   end
@@ -29,6 +37,12 @@ class Transactions::CategoriesController < ApplicationController
   private
     def set_category
       @category = Current.family.transaction_categories.find(params[:id])
+    end
+
+    def set_transaction
+      if params[:transaction_id]
+        @transaction = Current.family.transactions.find(params[:transaction_id])
+      end
     end
 
     def category_params
