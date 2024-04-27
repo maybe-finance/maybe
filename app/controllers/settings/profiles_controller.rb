56a18f71dbc6706d6453e10f7d8a290178b12868
Christian Robinson <christian@robbo.dev>
2024-04-27 09:20:12 +0100

    Purge image when form is blank

--- app/controllers/settings/profiles_controller.rb
+++ app/controllers/settings/profiles_controller.rb
@@ -5,6 +5,10 @@ class Settings::ProfilesController < ApplicationController
   def update
     user_params_with_family = user_params
 
+    if params[:user][:profile_image].blank?
+      Current.user.profile_image.purge
+    end
+
     if Current.family && user_params_with_family[:family_attributes]
       family_attributes = user_params_with_family[:family_attributes].merge({ id: Current.family.id })
       user_params_with_family[:family_attributes] = family_attributes
