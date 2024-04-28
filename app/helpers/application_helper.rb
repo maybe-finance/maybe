45bacdb9660fb3e5f9bd2f98f2a49b7f5050e1b7
Jakub Kottnauer <jk@jakubkottnauer.com>
2024-04-28 10:26:13 +0200

    Add stimulus controller to update avatar in merchant form

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -22,9 +22,9 @@ module ApplicationHelper
 
   # Wrap view with <%= modal do %> ... <% end %> to have it open in a modal
   # Make sure to add data-turbo-frame="modal" to the link/button that opens the modal
-  def modal(classes = "", &block)
+  def modal(options = {}, &block)
     content = capture &block
-    render partial: "shared/modal", locals: { content:, classes: }
+    render partial: "shared/modal", locals: { content:, classes: options[:classes] }
   end
 
   def account_groups
