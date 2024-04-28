a2dcb521f9180249b261dccd9a4cad6ee9ce5812
TiboGrx <gorioux.thibaut@gmail.com>
2024-04-28 09:34:08 +0200

    Test WIP

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -2,22 +2,20 @@ class ApplicationMailer < ActionMailer::Base
   default from: ENV["EMAIL_SENDER"]
   layout "mailer"
 
-  before_action :set_self_host_settings, if: -> { ENV["SELF_HOSTING_ENABLED"] == "true" }
+  before_action :set_self_host_email_sender, if: -> { ENV["SELF_HOSTING_ENABLED"] == "true" }
+  after_action :set_self_host_smtp_settings, if: -> { ENV["SELF_HOSTING_ENABLED"] == "true" && Rails.application.config.action_mailer.delivery_method = :smtp }
 
   private
 
-  def set_self_host_settings
-    ActionMailer::Base.default(from: Setting.email_sender)
-    if Rails.application.config.action_mailer.delivery_method = :smtp
-      set_self_host_smtp_settings
-    end
+  def set_self_host_email_sender
+    self.class.default(from: Setting.email_sender)
   end
 
   def set_self_host_smtp_settings
-    ActionMailer::Base.smtp_settings = { address: Setting.smtp_host,
-                                         port: Setting.smtp_port,
-                                         user_name: Setting.smtp_username,
-                                         password: Setting.smtp_password,
-                                         tls: ENV["SMTP_TLS_ENABLED"] == "true" }
+    mail.delivery_method.settings.merge!({ address: Setting.smtp_host,
+                                           port: Setting.smtp_port,
+                                           user_name: Setting.smtp_username,
+                                           password: Setting.smtp_password,
+                                           tls: ENV["SMTP_TLS_ENABLED"] == "true" })
   end
 end
