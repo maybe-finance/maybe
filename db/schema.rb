c48cba1f55cafdd349e87a6b8c32fb1e1576d8e3
Jemilu Mohammed <jemilu@crowncitytechnologies.com>
2024-04-30 14:15:15 +0000

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_04_26_191312) do
+ActiveRecord::Schema[7.2].define(version: 2024_04_27_171242) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
