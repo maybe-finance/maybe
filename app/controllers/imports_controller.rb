025bd97e7a6ad85fe53508f351b32037d5dfd0d4
Zach Gollwitzer <zach.gollwitzer@gmail.com>
2024-05-02 18:37:57 -0400

    Safely handle field mappings

--- app/controllers/imports_controller.rb
+++ app/controllers/imports_controller.rb
@@ -1,45 +1,33 @@
 class ImportsController < ApplicationController
   before_action :set_import, only: %i[ show edit update destroy ]
 
-  # GET /imports
   def index
     @imports = Import.all
   end
 
-  # GET /imports/1
   def show
   end
 
-  # GET /imports/new
   def new
     @import = Import.new
   end
 
-  # GET /imports/1/edit
   def edit
   end
 
-  # POST /imports
   def create
-    @import = Import.new(import_params)
+    account = Current.family.accounts.find(params[:import][:account_id])
+    @import = Import.create!(account: account)
 
-    if @import.save
-      redirect_to @import, notice: "Import was successfully created."
-    else
-      render :new, status: :unprocessable_entity
-    end
+    redirect_to @import, notice: "Import was successfully created."
   end
 
-  # PATCH/PUT /imports/1
   def update
-    if @import.update(import_params)
-      redirect_to @import, notice: "Import was successfully updated.", status: :see_other
-    else
-      render :edit, status: :unprocessable_entity
-    end
+    # TODO: handle mappings safely
+    @import = Current.family.imports.find(params[:id])
+    redirect_to @import, notice: "Import was successfully updated.", status: :see_other
   end
 
-  # DELETE /imports/1
   def destroy
     @import.destroy!
     redirect_to imports_url, notice: "Import was successfully destroyed.", status: :see_other
@@ -47,13 +35,7 @@ class ImportsController < ApplicationController
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_import
     @import = Import.find(params[:id])
-  end
-
-  # Only allow a list of trusted parameters through.
-  def import_params
-    params.require(:import).permit(:account_id, :column_mappings)
   end
 end
