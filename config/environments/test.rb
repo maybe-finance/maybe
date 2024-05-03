63cd5691840ddbf0e0339622057387c0a41274d0
Zach Gollwitzer <zach.gollwitzer@gmail.com>
2024-05-02 21:57:20 -0400

    Use `:test` adapter in test

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -58,6 +58,8 @@ Rails.application.configure do
   # Annotate rendered view with file names.
   # config.action_view.annotate_rendered_view_with_filenames = true
 
+  config.active_job.queue_adapter = :test
+
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
 end
