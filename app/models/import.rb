e578350471014d1b8033c0d0dc7e42744a12cebb
Zach Gollwitzer <zach.gollwitzer@gmail.com>
2024-05-09 10:58:56 -0400

    Add complete? status to Import

--- app/models/import.rb
+++ app/models/import.rb
@@ -2,9 +2,22 @@ class Import < ApplicationRecord
   belongs_to :account
   has_many :rows, dependent: :destroy
   validate :raw_csv_must_be_valid_csv
+  before_update :prevent_update_after_complete
   scope :ordered, -> { order(:created_at) }
 
+  def complete?
+    # Interim placeholder
+    false
+  end
+
   private
+
+    def prevent_update_after_complete
+      if complete?
+        errors.add(:base, "Update not allowed on a completed import.")
+        throw(:abort)
+      end
+    end
 
     def raw_csv_must_be_valid_csv
       return if raw_csv.nil?
--- test/models/import_test.rb
+++ test/models/import_test.rb
@@ -3,6 +3,18 @@ require "test_helper"
 class ImportTest < ActiveSupport::TestCase
   setup do
     @import = imports(:empty_import)
+
+    @valid_csv = <<-ROWS
+      name,age
+      John,20
+      Jane,23
+    ROWS
+
+    @invalid_csv = <<-ROWS
+      name,age
+      "John Doe,23
+      "Jane Doe",25
+    ROWS
   end
 
   test "raw csv input cannot be empty" do
@@ -11,22 +23,25 @@ class ImportTest < ActiveSupport::TestCase
   end
 
   test "raw csv input must conform to csv spec" do
-    invalid_csv_format = <<-ROWS
-      name,age
-      "John Doe,23
-      "Jane Doe",25
-    ROWS
-
-    @import.raw_csv = invalid_csv_format
+    @import.raw_csv = @invalid_csv
     assert_not @import.valid?
 
-    valid_csv_format = <<-ROWS
-      name,age
-      John,20
-      Jane,23
-    ROWS
-
-    @import.raw_csv = valid_csv_format
+    @import.raw_csv = @valid_csv
     assert @import.valid?
+  end
+
+  test "cannot modify a completed import" do
+    import = imports(:completed_import)
+
+    # Placeholder implementation
+    import.stubs(:complete?).returns(true)
+
+    assert import.complete?
+    import.raw_csv = @valid_csv
+
+    saved = import.save
+
+    assert_not saved
+    assert_includes import.errors.full_messages, "Update not allowed on a completed import."
   end
 end
