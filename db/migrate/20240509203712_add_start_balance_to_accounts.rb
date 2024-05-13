2c7c7bbe70c66c5adefeccb99fd2c1531e03ec67
Jakub Kottnauer <jk@jakubkottnauer.com>
2024-05-13 08:58:49 +0200

    Do not populate start_balance in migration

--- db/migrate/20240509203712_add_start_balance_to_accounts.rb
+++ db/migrate/20240509203712_add_start_balance_to_accounts.rb
@@ -1,11 +1,5 @@
 class AddStartBalanceToAccounts < ActiveRecord::Migration[7.2]
   def change
     add_column :accounts, :start_balance, :decimal, precision: 19, scale: 4
-
-    reversible do |direction|
-      direction.up do
-        Account.update_all("start_balance=balance")
-      end
-    end
   end
 end
