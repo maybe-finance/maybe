cc520884f4e46ae41a6f0400f375cc224e099cff
Zach Gollwitzer <zach.gollwitzer@gmail.com>
2024-05-17 16:44:43 -0400

    Fix auto upgrade logic

--- app/helpers/upgrades_helper.rb
+++ app/helpers/upgrades_helper.rb
@@ -1,12 +1,12 @@
 module UpgradesHelper
-  def upgrade_notification
+  def get_upgrade_for_notification(user, upgrades_mode)
     return nil unless ENV["UPGRADES_ENABLED"] == "true"
 
     completed_upgrade = Upgrader.completed_upgrade
-    return completed_upgrade if completed_upgrade && Current.user.last_alerted_upgrade_commit_sha != completed_upgrade.commit_sha
+    return completed_upgrade if completed_upgrade && user.last_alerted_upgrade_commit_sha != completed_upgrade.commit_sha
 
     available_upgrade = Upgrader.available_upgrade
-    if available_upgrade && Setting.upgrades_mode == "manual" && Current.user.last_prompted_upgrade_commit_sha != available_upgrade.commit_sha
+    if available_upgrade && upgrades_mode == "manual" && user.last_prompted_upgrade_commit_sha != available_upgrade.commit_sha
       available_upgrade
     end
   end
