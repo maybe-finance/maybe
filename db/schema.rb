94345ddc3ac37ca2ec79072f71daa1a8b942d72a
Jakub Kottnauer <jk@jakubkottnauer.com>
2024-05-20 17:33:19 +0200

    Add migration to make all current users admins (#770)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_05_02_205006) do
+ActiveRecord::Schema[7.2].define(version: 2024_05_20_074309) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
