5d90f4a8dfd71c155a4c71ffb9c3a2356618b757
Yaavi <hi@yaavi.me>
2024-06-19 17:02:30 +0800

    fix: default_locale en

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,7 +81,7 @@ Rails.application.configure do
   # config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
 
   # Set default locale
-  config.i18n.default_locale = :zh
+  config.i18n.default_locale = :en
 
   # Ensure available locales include your desired languages
   config.i18n.available_locales = [:en, :zh]
