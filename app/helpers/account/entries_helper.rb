751716add065c3e3d2dd3b23bf526606d09c8426
Zach Gollwitzer <zach.gollwitzer@gmail.com>
2024-06-27 14:58:43 -0400

    Add permitted partial key helper

--- app/helpers/account/entries_helper.rb
+++ app/helpers/account/entries_helper.rb
@@ -1,2 +1,6 @@
 module Account::EntriesHelper
+  def permitted_entryable_key(entry)
+    permitted_entryables = %w[transaction valuation]
+    entry.entryable_name_short.presence_in(permitted_entryables)
+  end
 end
