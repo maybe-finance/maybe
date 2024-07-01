32b2cecd13b2432a92f274c6f8312cc65c4deb5e
Zach Gollwitzer <zach.gollwitzer@gmail.com>
2024-07-01 10:05:42 -0400

    Remove more unused code

--- app/models/account/entryable.rb
+++ app/models/account/entryable.rb
@@ -9,8 +9,5 @@ module Account::Entryable
 
   included do
     has_one :entry, as: :entryable, touch: true
-
-    scope :with_entry, -> { includes(:entry) }
-    scope :ordered_with_entry, -> { joins(:entry).order("account_entries.date DESC, account_entries.id ASC") }
   end
 end
--- app/models/account/valuation.rb
+++ app/models/account/valuation.rb
@@ -1,10 +1,6 @@
 class Account::Valuation < ApplicationRecord
   include Account::Entryable
 
-  def trend
-    @trend ||= create_trend
-  end
-
   class << self
     def search(_params)
       all
@@ -14,22 +10,4 @@ class Account::Valuation < ApplicationRecord
       false
     end
   end
-
-  private
-
-    def previous_valuation
-      @previous_valuation ||= self.entry.account
-                                  .valuations
-                                  .with_entry
-                                  .where("date < ?", self.entry.date)
-                                  .order(date: :desc)
-                                  .first
-    end
-
-    def create_trend
-      TimeSeries::Trend.new \
-        current: self.entry.amount_money,
-        previous: previous_valuation&.entry&.amount_money,
-        favorable_direction: self.entry.account.favorable_direction
-    end
 end
