0e231de47864b0419cd9ee6997673c47d1760c36
Magnus Jensen <magnushjensen.mail@gmail.com>
2024-07-06 13:29:09 +0200

    add stimulus controller for reactive validation

---
+++ app/javascript/controllers/validation_controller.js
@@ -0,0 +1,48 @@
+import { Controller } from "@hotwired/stimulus";
+
+// Connects to data-controller="validation"
+export default class extends Controller {
+  static classes = ["invalid"];
+  static targets = ["empty", "submit"];
+  static values = { isValid: { type: Boolean, default: false } };
+
+  isValidValueChanged() {
+    if (!this.hasSubmitTarget) return;
+
+    if (this.isValidValue) {
+      this._makeSubmitValid();
+    } else {
+      this._makeSubmitInvalid();
+    }
+  }
+
+  submitTargetConnected() {
+    if (this.isValidValue) {
+      this._makeSubmitValid();
+    } else {
+      this._makeSubmitInvalid();
+    }
+  }
+
+  validate() {
+    // Add the validation targets and their respective logic here.
+    for (const target of this.emptyTargets) {
+      if (target.value === "") {
+        this.isValidValue = false;
+        return;
+      }
+    }
+
+    this.isValidValue = true;
+  }
+
+  _makeSubmitValid() {
+    this.element.classList.remove(this.invalidClass);
+    this.submitTarget.disabled = false;
+  }
+
+  _makeSubmitInvalid() {
+    this.element.classList.add(this.invalidClass);
+    this.submitTarget.disabled = true;
+  }
+}
