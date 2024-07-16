e51806b98b1cbde4b2d3b35d4fc5973eceaf0f7a
Zach Gollwitzer <zach@maybe.co>
2024-07-16 14:08:24 -0400

    More composable forms (#989)
    * Make forms more composable, opt-in to form builder
    
    * Remove unused method
    
    * Simpler money input controls
    
    * Add in new form styling to imports
    
    * Lint fixes
    
    * Small tweak of multi select styles

--- app/helpers/forms_helper.rb
+++ app/helpers/forms_helper.rb
@@ -1,7 +1,12 @@
 module FormsHelper
+  def styled_form_with(**options, &block)
+    options[:builder] = StyledFormBuilder
+    form_with(**options, &block)
+  end
+
   def form_field_tag(options = {}, &block)
     options[:class] = [ "form-field", options[:class] ].compact.join(" ")
-    tag.div **options, &block
+    tag.div(**options, &block)
   end
 
   def radio_tab_tag(form:, name:, value:, label:, icon:, checked: false, disabled: false)
@@ -11,23 +16,59 @@ module FormsHelper
     end
   end
 
-  def selectable_categories
-    Current.family.categories.alphabetically
+  def period_select(form:, selected:, classes: "border border-alpha-black-100 shadow-xs rounded-lg text-sm pr-7 cursor-pointer text-gray-900 focus:outline-none focus:ring-0")
+    periods_for_select = [ [ "7D", "last_7_days" ], [ "1M", "last_30_days" ], [ "1Y", "last_365_days" ], [ "All", "all" ] ]
+    form.select(:period, periods_for_select, { selected: selected }, class: classes, data: { "auto-submit-form-target": "auto" })
   end
 
-  def selectable_merchants
-    Current.family.merchants.alphabetically
+  def money_with_currency_field(form, money_method, options = {})
+    render partial: "shared/money_field", locals: {
+      form: form,
+      money_method: money_method,
+      disable_currency: options[:disable_currency] || false,
+      hide_currency: options[:hide_currency] || false,
+      label: options[:label] || "Amount"
+    }
   end
 
-  def selectable_accounts
-    Current.family.accounts.alphabetically
+  def money_field(form, method, options = {})
+    value = form.object.send(method)
+
+    currency = value&.currency || Money::Currency.new(options[:default_currency] || "USD")
+
+    # See "Monetizable" concern
+    money_amount_method = method.to_s.chomp("_money").to_sym
+
+    money_options = {
+      value: value&.amount,
+      placeholder: 100,
+      min: -99999999999999,
+      max: 99999999999999,
+      step: currency.step
+    }
+
+    merged_options = options.merge(money_options)
+
+    form.number_field money_amount_method, merged_options
   end
 
-  def selectable_tags
-    Current.family.tags.alphabetically.pluck(:name, :id)
+  def currency_select_full(form, method, options = {}, html_options = {}, &block)
+    choices = currencies_for_select.map { |currency| [ "#{currency.name} (#{currency.iso_code})", currency.iso_code ] }
+    form.select method, choices, options, html_options, &block
+  end
+
+  def currency_select(form, method, options = {}, html_options = {}, &block)
+    choices = currencies_for_select.map(&:iso_code)
+    form.select method, choices, options, html_options, &block
   end
 
   private
+
+    def currencies_for_select
+      Money::Currency.all_instances
+                     .sort_by(&:priority)
+    end
+
     def radio_tab_contents(label:, icon:)
       tag.div(class: "flex px-4 py-1 rounded-lg items-center space-x-2 justify-center text-gray-400 group-has-[:checked]:bg-white group-has-[:checked]:text-gray-800 group-has-[:checked]:shadow-sm") do
         concat lucide_icon(icon, class: "w-5 h-5")
---
+++ app/helpers/styled_form_builder.rb
@@ -0,0 +1,55 @@
+class StyledFormBuilder < ActionView::Helpers::FormBuilder
+  # Fields that visually inherit from "text field"
+  class_attribute :text_field_helpers, default: field_helpers - [ :label, :check_box, :radio_button, :fields_for, :fields, :hidden_field, :file_field ]
+
+  # Wraps "text" inputs with custom structure + base styles
+  text_field_helpers.each do |selector|
+    class_eval <<-RUBY_EVAL, __FILE__, __LINE__ + 1
+      def #{selector}(method, options = {})
+        input_html = label_html(method, options) + super(method, merged_options(options))
+        input_html = apply_form_field_wrapper(input_html) unless options[:inline]
+        input_html
+      end
+    RUBY_EVAL
+  end
+
+  def radio_button(method, tag_value, options = {})
+    super(method, tag_value, merged_options(options, "form-field__radio"))
+  end
+
+  def select(method, choices, options = {}, html_options = {})
+    input_html = label_html(method, options) + super(method, choices, options, merged_options(html_options))
+    input_html = apply_form_field_wrapper(input_html, class: "pr-0") unless options[:inline]
+    input_html
+  end
+
+  def collection_select(method, collection, value_method, text_method, options = {}, html_options = {})
+    input_html = label_html(method, options) + super(method, collection, value_method, text_method, options, merged_options(html_options))
+    input_html = apply_form_field_wrapper(input_html, class: "pr-0") unless options[:inline]
+    input_html
+  end
+
+  def submit(value = nil, options = {})
+    value, options = nil, value if value.is_a?(Hash)
+    super(value, merged_options(options, "form-field__submit"))
+  end
+
+  private
+
+    def apply_form_field_wrapper(input_html, **options)
+      @template.form_field_tag(**options) do
+        input_html
+      end
+    end
+
+    def merged_options(options, default_class = "form-field__input")
+      combined_classes = options.fetch(:class, "") + " #{default_class}"
+      style_options = { class: combined_classes }
+      non_custom_options = options.except(:class, :label, :inline)
+      style_options.merge(non_custom_options)
+    end
+
+    def label_html(method, options)
+      options[:label] ? label(method, options[:label], class: "form-field__label") : "".html_safe
+    end
+end
