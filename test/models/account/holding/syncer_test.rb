8ab025807cb6439868c2da30926cb05d6afb3d06
Tony Yesudas <tonyvince7@gmail.com>
2024-08-09 11:31:18 +0200

    Fix my stupidity

--- test/models/account/holding/syncer_test.rb
+++ test/models/account/holding/syncer_test.rb
@@ -103,8 +103,8 @@ class Account::Holding::SyncerTest < ActiveSupport::TestCase
 
         assert actual_holding, "expected #{ticker} holding on date: #{date}"
         assert_equal expected_holding[:qty], actual_holding.qty, "expected #{expected_qty} qty for holding #{ticker} on date: #{date}"
-        assert_equal expected_holding[:amount].to_i, actual_holding.amount.to_i, "expected #{expected_amount} amount for holding #{ticker} on date: #{date}"
-        assert_equal expected_holding[:price].to_i, actual_holding.price.to_i, "expected #{expected_price} price for holding #{ticker} on date: #{date}"
+        assert_equal expected_holding[:amount].to_d, actual_holding.amount.to_d, "expected #{expected_amount} amount for holding #{ticker} on date: #{date}"
+        assert_equal expected_holding[:price].to_d, actual_holding.price.to_d, "expected #{expected_price} price for holding #{ticker} on date: #{date}"
       end
     end
 
