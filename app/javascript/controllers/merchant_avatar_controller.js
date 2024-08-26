ef35f4a2ae8057bb56089020fa5cbb2a83f5fac4
Zach Gollwitzer <zach.gollwitzer@gmail.com>
2024-08-26 16:55:08 -0400

    Fix merchants color picker

--- app/javascript/controllers/merchant_avatar_controller.js
+++ app/javascript/controllers/merchant_avatar_controller.js
@@ -1,29 +1,26 @@
-import {Controller} from "@hotwired/stimulus";
+import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="merchant-avatar"
 // Used by the transaction merchant form to show a preview of what the avatar will look like
 export default class extends Controller {
   static targets = [
     "name",
-    "color",
     "avatar"
   ];
 
   connect() {
     this.nameTarget.addEventListener("input", this.handleNameChange);
-    this.colorTarget.addEventListener("input", this.handleColorChange);
   }
 
   disconnect() {
     this.nameTarget.removeEventListener("input", this.handleNameChange);
-    this.colorTarget.removeEventListener("input", this.handleColorChange);
   }
 
   handleNameChange = (e) => {
     this.avatarTarget.textContent = (e.currentTarget.value?.[0] || "?").toUpperCase();
   }
 
-  handleColorChange = (e) => {
+  handleColorChange(e) {
     const color = e.currentTarget.value;
     this.avatarTarget.style.backgroundColor = `color-mix(in srgb, ${color} 5%, white)`;
     this.avatarTarget.style.borderColor = `color-mix(in srgb, ${color} 10%, white)`;
