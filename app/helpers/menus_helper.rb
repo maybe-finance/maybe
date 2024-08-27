f16c43fa249c68451e258038edf51aef2aab5e21
Zach Gollwitzer <zach.gollwitzer@gmail.com>
2024-08-27 16:17:45 -0400

    Categories, tags, merchants, and menus improvements

--- app/helpers/menus_helper.rb
+++ app/helpers/menus_helper.rb
@@ -1,6 +1,6 @@
 module MenusHelper
   def contextual_menu(&block)
-    tag.div class: "relative cursor-pointer", data: { controller: "menu" } do
+    tag.div data: { controller: "menu" } do
       concat contextual_menu_icon
       concat contextual_menu_content(&block)
     end
@@ -25,13 +25,14 @@ module MenusHelper
 
   private
     def contextual_menu_icon
-      tag.button class: "flex hover:bg-gray-100 p-2 rounded", data: { menu_target: "button" } do
+      tag.button class: "flex hover:bg-gray-100 p-2 rounded cursor-pointer", data: { menu_target: "button" } do
         lucide_icon "more-horizontal", class: "w-5 h-5 text-gray-500"
       end
     end
 
     def contextual_menu_content(&block)
-      tag.div class: "absolute z-10 top-10 right-0 border border-alpha-black-25 bg-white rounded-lg shadow-xs hidden", data: { menu_target: "content" } do
+      tag.div class: "z-50 border border-alpha-black-25 bg-white rounded-lg shadow-xs hidden",
+               data: { menu_target: "content" } do
         capture(&block)
       end
     end
