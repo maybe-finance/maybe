24833d15017a2035a9e2a491c99da8b3ac5a1b48
Zach Gollwitzer <zach.gollwitzer@gmail.com>
2024-09-09 14:44:22 -0400

    Only show latest release on changelog

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -31,7 +31,7 @@ class PagesController < ApplicationController
   end
 
   def changelog
-    @releases_notes = Provider::Github.new.fetch_latest_releases_notes
+    @release_notes = Provider::Github.new.fetch_latest_releases_notes
   end
 
   def feedback
