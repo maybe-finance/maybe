af202750aaba6a19ca07f4bca8cd3106d2a5af24
Zach Gollwitzer <zach.gollwitzer@gmail.com>
2024-09-30 14:44:44 -0400

    Add back row validations

--- app/models/import/row.rb
+++ app/models/import/row.rb
@@ -1,6 +1,10 @@
 class Import::Row < ApplicationRecord
   belongs_to :import
 
+  validates :date, presence: true, if: -> { import.type in ["TransactionImport", "TradeImport", "MintImport"] }
+  validates :amount, numericality: true
+  validate :date_matches_user_format
+
   scope :ordered, -> { order(:id) }
 
   def tags_list
@@ -17,4 +21,14 @@ class Import::Row < ApplicationRecord
     Import::AccountMapping.sync(import)
     Import::AccountTypeMapping.sync(import)
   end
+
+  private
+    def date_matches_user_format
+      return if date.blank?
+
+      parsed_date = Date.strptime(date, import.date_format) rescue nil
+      if parsed_date.nil?
+        errors.add(:date, "must exactly match the format: #{import.date_format}")
+      end
+    end
 end
