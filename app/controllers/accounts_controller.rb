e8eb4b0eb964c1ecf83fbaef51e46de2221695c1
Zach Gollwitzer <zach.gollwitzer@gmail.com>
2024-10-01 10:44:47 -0400

    Currency preset for new accounts

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -23,7 +23,10 @@ class AccountsController < ApplicationController
   end
 
   def new
-    @account = Account.new(accountable: Accountable.from_type(params[:type])&.new)
+    @account = Account.new(
+      accountable: Accountable.from_type(params[:type])&.new,
+      currency: Current.family.currency
+    )
 
     @account.accountable.address = Address.new if @account.accountable.is_a?(Property)
 
