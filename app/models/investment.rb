ea7b6f336c75c536da085f25aec50c8e6f354266
hatz <hatz@hey.com>
2024-10-10 12:31:38 -0500

    Remove cryptocurrency investment subtype

--- app/models/investment.rb
+++ app/models/investment.rb
@@ -7,14 +7,14 @@ class Investment < ApplicationRecord
     [ "Pension", "pension" ],
     [ "Retirement", "retirement" ],
     [ "401(k)", "401k" ],
+    [ "Traditional 401(k)", "traditional_401k" ],
     [ "Roth 401(k)", "roth_401k" ],
     [ "529 Plan", "529_plan" ],
     [ "Health Savings Account", "hsa" ],
     [ "Mutual Fund", "mutual_fund" ],
     [ "Traditional IRA", "traditional_ira" ],
     [ "Roth IRA", "roth_ira" ],
-    [ "Angel", "angel" ],
-    [ "Cryptocurrency", "cryptocurrency" ]
+    [ "Angel", "angel" ]
   ].freeze
 
   def value
