29be37c8f2a6afe3fdfd8202f3a69859dc17dee8
Zach Gollwitzer <zach.gollwitzer@gmail.com>
2024-10-24 09:01:07 -0400

    Add upgrade modal prompt

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,12 @@ class ApplicationController < ActionController::Base
   include Onboardable, Localize, AutoSync, Authentication, Invitable, SelfHostable, StoreLocation, Impersonatable
   include Pagy::Backend
 
+  helper_method :require_upgrade?
+
   private
+    def require_upgrade?
+      !Current.family&.subscribed? && !self_hosted?
+    end
 
     def with_sidebar
       return "turbo_rails/frame" if turbo_frame_request?
