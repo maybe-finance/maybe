3f7c31c13edfee00de66725b0bde95d2e8ccd7a1
yash-learner <yash.topaz@gmail.com>
2024-10-24 17:43:45 +0530

    Quick Try: Add event listener to submit button and disable it

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -5,6 +5,15 @@ export default class extends Controller {
   connect() {
     if (this.element.open) return;
     this.element.showModal();
+
+    // Add event listener to disable submit button on form submission
+    const submitButton = this.element.querySelector("#submit-button");
+    if (submitButton) {
+      submitButton.addEventListener(
+        "click",
+        this.disableSubmitButton.bind(this)
+      );
+    }
   }
 
   // Hide the dialog when the user clicks outside of it
@@ -16,5 +25,12 @@ export default class extends Controller {
 
   close() {
     this.element.close();
+  }
+
+  disableSubmitButton(event) {
+    const submitButton = event.target;
+    submitButton.disabled = true;
+    submitButton.value = "Creating...";
+    submitButton.form.submit();
   }
 }
