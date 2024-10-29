f8e58d689383bb5b4a440e494d4e4bddd6712fa7
Josh Pigford <josh@joshpigford.com>
2024-10-29 19:52:25 -0400

    Linter

--- app/models/account/trade_builder.rb
+++ app/models/account/trade_builder.rb
@@ -32,7 +32,7 @@ class Account::TradeBuilder < Account::EntryBuilder
 
     def security
       exchange_mic = ticker.match(/\((.*?)\)/)&.captures&.first
-      ticker_symbol = ticker.gsub(/\s*\(.*?\)\s*/, '')
+      ticker_symbol = ticker.gsub(/\s*\(.*?\)\s*/, "")
 
       Security.find_or_create_by(ticker: ticker_symbol, exchange_mic: exchange_mic)
     end
