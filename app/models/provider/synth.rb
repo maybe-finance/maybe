959b18f8bdc8783000fd0b6d61912d01877b478d
Josh Pigford <josh@joshpigford.com>
2024-10-29 12:03:01 -0400

    Synth security fetching does better with a mic_code

--- app/models/provider/synth.rb
+++ app/models/provider/synth.rb
@@ -42,9 +42,10 @@ class Provider::Synth
     )
   end
 
-  def fetch_security_prices(ticker:, start_date:, end_date:)
+  def fetch_security_prices(ticker:, mic_code:, start_date:, end_date:)
     prices = paginate(
       "#{base_url}/tickers/#{ticker}/open-close",
+      mic_code: mic_code,
       start_date: start_date,
       end_date: end_date
     ) do |body|
