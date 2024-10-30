4a3c331e20ca3b665c1fa917adbf35004fb5a228
Josh Pigford <josh@joshpigford.com>
2024-10-30 08:17:17 -0400

    Lint

--- db/migrate/20241030121302_fix_not_null_stock_exchange_data.rb
+++ db/migrate/20241030121302_fix_not_null_stock_exchange_data.rb
@@ -4,7 +4,7 @@ class FixNotNullStockExchangeData < ActiveRecord::Migration[7.2]
     change_column_null :stock_exchanges, :currency_symbol, true
     change_column_null :stock_exchanges, :currency_name, true
     change_column_null :stock_exchanges, :city, true
-    change_column_null :stock_exchanges, :timezone_name, true 
+    change_column_null :stock_exchanges, :timezone_name, true
     change_column_null :stock_exchanges, :timezone_abbr, true
   end
 end
