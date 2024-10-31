5008497300513d9675fbe1aac001877242089281
Josh Pigford <josh@joshpigford.com>
2024-10-30 20:20:47 -0400

    Security cleanup

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -8,6 +8,10 @@ class InvitationsController < ApplicationController
     @invitation = Current.family.invitations.build(invitation_params)
     @invitation.inviter = Current.user
 
+    if @invitation.role == "admin" && !Current.user.admin?
+      @invitation.role = "member"
+    end
+
     if @invitation.save
       InvitationMailer.invite_email(@invitation).deliver_later
       flash[:notice] = t(".success")
@@ -26,6 +30,14 @@ class InvitationsController < ApplicationController
   private
 
     def invitation_params
-      params.require(:invitation).permit(:email, :role)
+      base_params = params.require(:invitation).permit(:email)
+      
+      if params[:invitation][:role].in?(%w[admin member])
+        base_params[:role] = params[:invitation][:role]
+      else
+        base_params[:role] = "member"
+      end
+      
+      base_params
     end
 end
