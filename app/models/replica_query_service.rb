ffa979c16f441d5ebde16426879eb8e196d7f82d
Josh Pigford <josh@joshpigford.com>
2024-11-04 12:38:51 -0600

    Replica query service

---
+++ app/models/replica_query_service.rb
@@ -0,0 +1,15 @@
+class ReplicaQueryService
+  class ReplicaConnection < ActiveRecord::Base
+    self.abstract_class = true
+  end
+
+  def self.execute(query)
+    ReplicaConnection.establish_connection(ENV["READONLY_DATABASE_URL"])
+    result = ReplicaConnection.connection.execute(query)
+
+    # Close the connection when done
+    ReplicaConnection.connection_pool.disconnect!
+
+    result
+  end
+end
