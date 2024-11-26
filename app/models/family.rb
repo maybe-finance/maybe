854f31d8dfec1d091be8f12cff47ea52410b3449
Nikhil Badyal <nikhill773384@gmail.com>
2024-11-26 23:04:13 +0530

    Updated usage check threshold to 100pc instead of 1

--- app/models/family.rb
+++ app/models/family.rb
@@ -154,7 +154,7 @@ class Family < ApplicationRecord
   end
 
   def synth_overage?
-    self.class.synth_provider && self.class.synth_provider.usage.utilization >= 1
+    self.class.synth_provider && self.class.synth_provider.usage.utilization >= 100
   end
 
   def subscribed?
