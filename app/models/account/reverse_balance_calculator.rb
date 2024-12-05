a8d02cafaa97902e5e586c6fb2fc938084b0e0e6
Zach Gollwitzer <zach.gollwitzer@gmail.com>
2024-12-05 12:12:20 -0500

    Reverse balance sync with holdings

--- app/models/account/reverse_balance_calculator.rb
+++ app/models/account/reverse_balance_calculator.rb
@@ -4,46 +4,70 @@ class Account::ReverseBalanceCalculator
   end
 
   def calculate
-    entries = account.entries.order(:date).to_a
+    entries = account.entries.order(:date).to_a.map do |e|
+      e.amount = e.amount_money.exchange_to(
+        account.currency,
+        date: e.date,
+        fallback_rate: 1
+      ).amount
+      e.currency = account.currency
+      e
+    end
 
-    prior_balance = account.balance
+    holdings = account.holdings.to_a.map do |h|
+      h.amount = h.amount_money.exchange_to(
+        account.currency,
+        date: h.date,
+        fallback_rate: 1
+      ).amount
+      h.currency = account.currency
+      h
+    end
 
-    Date.current.downto(entries.first&.date || Date.current).map do |date|
+    prior_balance = account.investment? ? account.investment.cash_balance : account.balance
+
+    oldest_date = entries.first ? entries.first.date - 1.day : Date.current
+
+    cash_balances = Date.current.downto(oldest_date).map do |date|
       entries_for_date = entries.select { |e| e.date == date }
-      current_balance = calculate_balance(prior_balance, entries_for_date) 
+      holdings_for_date = holdings.select { |h| h.date == date }
+
+      valuation = entries_for_date.find { |e| e.account_valuation? }
+      
+      current_balance = if valuation 
+        # To get this to a cash valuation, we back out holdings value on day
+        valuation.amount - holdings_for_date.sum(&:amount)
+      else
+        transactions = entries_for_date.select { |e| e.account_transaction? }
+
+        calculate_balance(prior_balance, transactions)
+      end
+
+      balance_record = Account::Balance.new(
+        account: account,
+        date: date,
+        balance: valuation&.amount || prior_balance,
+        currency: account.currency
+      )
 
       prior_balance = current_balance
 
-      Account::Balance.new(
-        account: account,
-        date: date,
-        balance: current_balance,
-        currency: account.currency
-      )
+      balance_record
+    end
+
+    # Now, apply the daily holding values to the calculated cash balances
+    cash_balances.map do |balance|
+      holdings_value = holdings.select { |h| h.date == balance.date }.sum(&:amount)
+      authoritative_holdings_value = balance.date == Date.current && account.investment? ? account.investment.holdings_balance : holdings_value
+      balance.balance = balance.balance + holdings_value
+      balance
     end
   end
 
   private
     attr_reader :account
 
-    def calculate_balance(prior_balance, entries)
-      normalized_entries = entries.map do |entry|
-        entry.amount = entry.amount_money.exchange_to(
-          account.currency,
-          date: entry.date,
-          fallback_rate: 1
-        ).amount
-
-        entry.currency = account.currency
-        entry
-      end
-
-      transactions = normalized_entries.select { |e| e.account_transaction? }
-      valuation = normalized_entries.find { |e| e.account_valuation? }
-
-      # Valuations take highest precedence
-      return valuation.amount if valuation
-
+    def calculate_balance(prior_balance, transactions)
       flows = transactions.sum(&:amount)
       flows *= -1 if account.liability?
       prior_balance + flows 
--- test/models/account/reverse_balance_calculator_test.rb
+++ test/models/account/reverse_balance_calculator_test.rb
@@ -8,7 +8,10 @@ class Account::ReverseBalanceCalculatorTest < ActiveSupport::TestCase
       name: "Test",
       balance: 20000,
       currency: "USD",
-      accountable: Investment.new
+      accountable: Investment.new(
+        cash_balance: 20000,
+        holdings_balance: 0 
+      )
     )
   end
 
@@ -25,7 +28,7 @@ class Account::ReverseBalanceCalculatorTest < ActiveSupport::TestCase
     create_valuation(account: @account, date: 4.days.ago.to_date, amount: 17000)
     create_valuation(account: @account, date: 2.days.ago.to_date, amount: 19000) 
 
-    expected = [ 17000, 19000,19000, 20000, 20000 ]
+    expected = [ 17000, 17000, 19000, 19000, 20000, 20000 ]
     calculated = chronological_balances_for(@account)
 
     assert_equal expected, calculated
@@ -41,10 +44,55 @@ class Account::ReverseBalanceCalculatorTest < ActiveSupport::TestCase
     assert_equal expected, calculated
   end
 
+  test "multi-entry sync" do 
+    create_transaction(account: @account, date: 8.days.ago.to_date, amount: -5000)
+    create_valuation(account: @account, date: 6.days.ago.to_date, amount: 17000)
+    create_transaction(account: @account, date: 6.days.ago.to_date, amount: -500)
+    create_transaction(account: @account, date: 4.days.ago.to_date, amount: -500)
+    create_valuation(account: @account, date: 3.days.ago.to_date, amount: 17000)
+    create_transaction(account: @account, date: 1.day.ago.to_date, amount: 100)
+
+    expected = [ 12000, 17000, 17000, 17000, 16500, 17000, 17000, 20100, 20000, 20000 ]
+    calculated = chronological_balances_for(@account)
+
+    assert_equal expected, calculated
+  end
+
+  test "investment balance sync" do 
+    @account.investment.update!(cash_balance: 18000, holdings_balance: 2000)
+    
+    create_transaction(account: @account, date: 4.days.ago.to_date, amount: 100)
+    create_transaction(account: @account, date: 2.days.ago.to_date, amount: -500) 
+
+    create_holding(date: Date.current, security: securities(:msft), amount: 2000)
+    create_holding(date: 1.day.ago.to_date, security: securities(:msft), amount: 1900)
+    create_holding(date: 2.days.ago.to_date, security: securities(:msft), amount: 1800)
+    create_holding(date: 3.days.ago.to_date, security: securities(:msft), amount: 1700)
+    create_holding(date: 4.days.ago.to_date, security: securities(:msft), amount: 1600)
+    create_holding(date: 5.days.ago.to_date, security: securities(:msft), amount: 1500)
+
+    expected = [ 19100, 19100, 19200, 19800, 19900, 20000 ]
+    calculated = chronological_balances_for(@account)
+
+    assert_equal expected, calculated
+  end
+
   private 
     def chronological_balances_for(account)
       series = Account::ReverseBalanceCalculator.new(account).calculate
 
       series.sort_by(&:date).map(&:balance)
+    end
+
+    def create_holding(date:, security:, amount:)
+      Account::Holding.create!(
+        account: @account,
+        security: security,
+        date: date,
+        qty: 0, # not used
+        price: 0, # not used
+        amount: amount,
+        currency: @account.currency 
+      )
     end
 end
