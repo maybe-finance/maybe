de78d5604e5acf0f8053646e9bdfd30c584521d1
Josh Pigford <josh@joshpigford.com>
2024-12-18 11:04:18 -0600

    Initial pass at Plaid EU

--- app/models/concerns/plaidable.rb
+++ app/models/concerns/plaidable.rb
@@ -5,10 +5,18 @@ module Plaidable
     def plaid_provider
       Provider::Plaid.new if Rails.application.config.plaid
     end
+
+    def plaid_eu_provider
+      Provider::Plaid.new if Rails.application.config.plaid_eu
+    end
   end
 
   private
     def plaid_provider
       self.class.plaid_provider
+    end
+
+    def plaid_eu_provider
+      self.class.plaid_eu_provider
     end
 end
