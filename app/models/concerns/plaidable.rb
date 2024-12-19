0c9cb333d5bbae0748379aef2532399a0eb1506a
Josh Pigford <josh@joshpigford.com>
2024-12-19 09:18:30 -0600

    Add EU support to Plaid Items

--- app/models/concerns/plaidable.rb
+++ app/models/concerns/plaidable.rb
@@ -9,14 +9,15 @@ module Plaidable
     def plaid_eu_provider
       Provider::Plaid.new if Rails.application.config.plaid_eu
     end
+
+    def plaid_provider_for(plaid_item)
+      return nil unless plaid_item
+      plaid_item.eu? ? plaid_eu_provider : plaid_provider
+    end
   end
 
   private
-    def plaid_provider
-      self.class.plaid_provider
-    end
-
-    def plaid_eu_provider
-      self.class.plaid_eu_provider
+    def plaid_provider_for(plaid_item)
+      self.class.plaid_provider_for(plaid_item)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_12_17_141716) do
+ActiveRecord::Schema[7.2].define(version: 2024_12_19_151540) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -532,6 +532,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_12_17_141716) do
     t.string "available_products", default: [], array: true
     t.string "billed_products", default: [], array: true
     t.datetime "last_synced_at"
+    t.string "plaid_region", default: "us", null: false
     t.index ["family_id"], name: "index_plaid_items_on_family_id"
   end
 
