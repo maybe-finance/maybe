5d10110693f83c1c67f5d2be4f3698fdefbf3ac2
Tony Vincent Yesudas <tonyvince7@gmail.com>
2025-01-09 17:45:03 +0100

    fix: Only admins can generate invite codes

--- app/controllers/invite_codes_controller.rb
+++ app/controllers/invite_codes_controller.rb
@@ -1,5 +1,6 @@
 class InviteCodesController < ApplicationController
   before_action :ensure_self_hosted
+  before_action :ensure_admin, only: :create
 
   def index
     @invite_codes = InviteCode.all
@@ -14,5 +15,9 @@ class InviteCodesController < ApplicationController
 
     def ensure_self_hosted
       redirect_to root_path unless self_hosted?
+    end
+
+    def ensure_admin
+      redirect_to settings_hosting_path, alert: "You are not allowed to generate invite codes" unless Current.user.admin?
     end
 end
