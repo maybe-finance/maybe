2809161ad5b3779cb597f19662fe6d55a0502769
Zach Gollwitzer <zach.gollwitzer@gmail.com>
2025-01-16 17:50:40 -0500

    Fix tests

--- test/models/transfer_test.rb
+++ test/models/transfer_test.rb
@@ -8,7 +8,7 @@ class TransferTest < ActiveSupport::TestCase
     @inflow = account_transactions(:transfer_in)
   end
 
-  test "auto matches transfers" do 
+  test "auto matches transfers" do
     outflow_entry = create_transaction(date: 1.day.ago.to_date, account: accounts(:depository), amount: 500)
     inflow_entry = create_transaction(date: Date.current, account: accounts(:credit_card), amount: -500)
 
