6a7fccef67e387307a73d78519378542989f4c11
MrTob <mr.tobtv@gmail.com>
2025-01-17 10:11:33 +0100

    FIX: correct display of percentages

--- app/models/family.rb
+++ app/models/family.rb
@@ -99,7 +99,7 @@ class Family < ApplicationRecord
 
     results.each do |r|
       r.define_singleton_method(:savings_rate) do
-        (income - spending) / income
+        (income - spending) / income * 100
       end
     end
 
@@ -130,7 +130,7 @@ class Family < ApplicationRecord
 
       savings << {
         date: r.date,
-        value: r.rolling_income != 0 ? (r.rolling_income - r.rolling_spend) / r.rolling_income : 0.to_d
+        value: r.rolling_income != 0 ? ((r.rolling_income - r.rolling_spend) / r.rolling_income * 100) : 0.to_d
       }
     end
 
