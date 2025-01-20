72fd17770734e731ca0acc0e87db198fce5f020e
Zach Gollwitzer <zach@maybe.co>
2025-01-20 15:12:53 -0500

    Do not raise on Plaid item not found exceptions for item deletions (#1646)

--- test/models/plaid_item_test.rb
+++ test/models/plaid_item_test.rb
@@ -18,4 +18,16 @@ class PlaidItemTest < ActiveSupport::TestCase
       @plaid_item.destroy
     end
   end
+
+  test "if plaid item not found, silently continues with deletion" do
+    @plaid_provider = mock
+
+    PlaidItem.stubs(:plaid_provider).returns(@plaid_provider)
+
+    @plaid_provider.expects(:remove_item).with(@plaid_item.access_token).raises(Plaid::ApiError.new("Item not found"))
+
+    assert_difference "PlaidItem.count", -1 do
+      @plaid_item.destroy
+    end
+  end
 end
