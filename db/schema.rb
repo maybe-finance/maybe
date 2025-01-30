e02da5881e7c9a0e55cfb06043948d974c798b4e
Josh Pigford <josh@joshpigford.com>
2025-01-30 14:29:11 -0600

    Schema reset

--- db/schema.rb
+++ db/schema.rb
@@ -102,7 +102,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_01_30_191533) do
     t.decimal "balance", precision: 19, scale: 4
     t.string "currency"
     t.boolean "is_active", default: true, null: false
-    t.virtual "classification", type: :string, as: "\nCASE\n    WHEN ((accountable_type)::text = ANY (ARRAY[('Loan'::character varying)::text, ('CreditCard'::character varying)::text, ('OtherLiability'::character varying)::text])) THEN 'liability'::text\n    ELSE 'asset'::text\nEND", stored: true
+    t.virtual "classification", type: :string, as: "\nCASE\n    WHEN ((accountable_type)::text = ANY ((ARRAY['Loan'::character varying, 'CreditCard'::character varying, 'OtherLiability'::character varying])::text[])) THEN 'liability'::text\n    ELSE 'asset'::text\nEND", stored: true
     t.uuid "import_id"
     t.uuid "plaid_account_id"
     t.boolean "scheduled_for_deletion", default: false
@@ -195,15 +195,6 @@ ActiveRecord::Schema[7.2].define(version: 2025_01_30_191533) do
     t.string "classification", default: "expense", null: false
     t.string "lucide_icon"
     t.index ["family_id"], name: "index_categories_on_family_id"
-  end
-
-  create_table "chats", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
-    t.uuid "user_id", null: false
-    t.string "title"
-    t.text "summary"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_chats_on_user_id"
   end
 
   create_table "credit_cards", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
@@ -488,33 +479,6 @@ ActiveRecord::Schema[7.2].define(version: 2025_01_30_191533) do
     t.index ["family_id"], name: "index_merchants_on_family_id"
   end
 
-  create_table "messages", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
-    t.uuid "chat_id", null: false
-    t.uuid "user_id"
-    t.text "content"
-    t.text "log"
-    t.string "role"
-    t.string "status", default: "pending"
-    t.boolean "hidden", default: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["chat_id"], name: "index_messages_on_chat_id"
-    t.index ["user_id"], name: "index_messages_on_user_id"
-  end
-
-  create_table "metrics", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
-    t.uuid "family_id", null: false
-    t.uuid "account_id"
-    t.string "kind", null: false
-    t.string "subkind"
-    t.date "date", null: false
-    t.decimal "value", precision: 10, scale: 2, null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["account_id"], name: "index_metrics_on_account_id"
-    t.index ["family_id"], name: "index_metrics_on_family_id"
-  end
-
   create_table "other_assets", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -552,7 +516,6 @@ ActiveRecord::Schema[7.2].define(version: 2025_01_30_191533) do
     t.string "available_products", default: [], array: true
     t.string "billed_products", default: [], array: true
     t.datetime "last_synced_at"
-    t.string "plaid_region", default: "us", null: false
     t.index ["family_id"], name: "index_plaid_items_on_family_id"
   end
 
@@ -733,7 +696,6 @@ ActiveRecord::Schema[7.2].define(version: 2025_01_30_191533) do
   add_foreign_key "budget_categories", "categories"
   add_foreign_key "budgets", "families"
   add_foreign_key "categories", "families"
-  add_foreign_key "chats", "users"
   add_foreign_key "impersonation_session_logs", "impersonation_sessions"
   add_foreign_key "impersonation_sessions", "users", column: "impersonated_id"
   add_foreign_key "impersonation_sessions", "users", column: "impersonator_id"
@@ -742,10 +704,6 @@ ActiveRecord::Schema[7.2].define(version: 2025_01_30_191533) do
   add_foreign_key "invitations", "families"
   add_foreign_key "invitations", "users", column: "inviter_id"
   add_foreign_key "merchants", "families"
-  add_foreign_key "messages", "chats"
-  add_foreign_key "messages", "users"
-  add_foreign_key "metrics", "accounts"
-  add_foreign_key "metrics", "families"
   add_foreign_key "plaid_accounts", "plaid_items"
   add_foreign_key "plaid_items", "families"
   add_foreign_key "rejected_transfers", "account_transactions", column: "inflow_transaction_id"
