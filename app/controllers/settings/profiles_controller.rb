50a460c80b7f95d7bb9b5b15194305bf448db11e
Josh Pigford <josh@joshpigford.com>
2025-01-30 12:59:05 -0600

    Add/remove members and invitations

--- app/controllers/settings/profiles_controller.rb
+++ app/controllers/settings/profiles_controller.rb
@@ -4,4 +4,28 @@ class Settings::ProfilesController < SettingsController
     @users = Current.family.users.order(:created_at)
     @pending_invitations = Current.family.invitations.pending
   end
+
+  def destroy
+    unless Current.user.admin?
+      flash[:alert] = t("settings.profiles.destroy.not_authorized")
+      redirect_to settings_profile_path
+      return
+    end
+
+    @user = Current.family.users.find(params[:user_id])
+
+    if @user == Current.user
+      flash[:alert] = t("settings.profiles.destroy.cannot_remove_self")
+      redirect_to settings_profile_path
+      return
+    end
+
+    if @user.destroy
+      flash[:notice] = t("settings.profiles.destroy.member_removed")
+    else
+      flash[:alert] = t("settings.profiles.destroy.member_removal_failed")
+    end
+
+    redirect_to settings_profile_path
+  end
 end
