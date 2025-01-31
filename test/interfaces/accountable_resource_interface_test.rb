53f4b32c330b107f5295105a78cb660b74a8deee
Zach Gollwitzer <zach@maybe.co>
2025-01-31 17:04:26 -0500

    Fix EU plaid flow (#1761)
    * Fix EU plaid flow
    
    * Fix failing tests

--- test/interfaces/accountable_resource_interface_test.rb
+++ test/interfaces/accountable_resource_interface_test.rb
@@ -4,9 +4,7 @@ module AccountableResourceInterfaceTest
   extend ActiveSupport::Testing::Declarative
 
   test "shows new form" do
-    Plaid::PlaidApi.any_instance.stubs(:link_token_create).returns(
-      Plaid::LinkTokenCreateResponse.new(link_token: "test-link-token")
-    )
+    Family.any_instance.stubs(:get_link_token).returns("test-link-token")
 
     get new_polymorphic_url(@account.accountable)
     assert_response :success
