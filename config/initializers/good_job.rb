283d9cd8e28d9f236b9f97c7134a32686ac6b058
Zach Gollwitzer <zach.gollwitzer@gmail.com>
2025-02-04 13:29:27 -0500

    Fix GoodJob config syntax

--- config/initializers/good_job.rb
+++ config/initializers/good_job.rb
@@ -14,12 +14,10 @@ Rails.application.configure do
   config.good_job.on_thread_error = ->(exception) { Rails.error.report(exception) }
 
   # 10 queue threads + 3 for job listener, cron, executor = 13 threads allocated
-  config.good_job.queues = {
-    "latency_low" => { max_threads: 3, priority: 10 }, # ~30s jobs
-    "latency_low,latency_medium" => { max_threads: 4, priority: 5 }, # ~1-2 min jobs
-    "latency_low,latency_medium,latency_high" => { max_threads: 2, priority: 1 }, # ~5+ min jobs
-    "*" => { max_threads: 1, priority: 0 } # fallback queue
-  }
+  # `latency_low` queue for jobs ~30s
+  # `latency_medium` queue for jobs ~1-2 min
+  # `latency_high` queue for jobs ~5+ min
+  config.good_job.queues = "latency_low:3;latency_low,latency_medium:4;latency_low,latency_medium,latency_high:2;*:1"
 
   # Auth for jobs admin dashboard
   ActiveSupport.on_load(:good_job_application_controller) do
