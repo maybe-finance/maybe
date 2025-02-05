d8a7bb1d4db606836092a3fd501943aa9364618d
Josh Pigford <josh@joshpigford.com>
2025-02-05 14:31:12 -0600

    Update config/initializers/openai.rb
    Co-authored-by: Chad Wilken <chad.wilken@gmail.com>
    Signed-off-by: Josh Pigford <josh@joshpigford.com>

--- config/initializers/openai.rb
+++ config/initializers/openai.rb
@@ -1,6 +1,6 @@
 if ENV.fetch("OPENAI_ACCESS_TOKEN", nil).present?
   OpenAI.configure do |config|
     config.access_token = ENV.fetch("OPENAI_ACCESS_TOKEN")
-    config.log_errors = true if Rails.env.development?
+    config.log_errors = Rails.env.development?
   end
 end
