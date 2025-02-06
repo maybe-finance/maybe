7ba9063e04d039ba20bdef211ab69b7e7a3271ee
Josh Pigford <josh@joshpigford.com>
2025-02-06 08:57:24 -0600

    feat: Add institution details to Plaid items (#1816)
    * feat: Add institution details to Plaid items
    
    - Fetch and store institution URL, ID, and primary color for Plaid items
    - Update PlaidItem model to retrieve and save institution metadata
    - Add new method in Plaid provider to get institution details
    - Update account logo view to use institution domain for logo generation
    
    * Add institution domain method to Account model
    
    - Extract institution domain logic from view to Account model
    - Simplify logo view by using new institution_domain method
    - Improve code reusability and separation of concerns

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_01_31_171943) do
+ActiveRecord::Schema[7.2].define(version: 2025_02_06_141452) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -517,6 +517,9 @@ ActiveRecord::Schema[7.2].define(version: 2025_01_31_171943) do
     t.string "billed_products", default: [], array: true
     t.datetime "last_synced_at"
     t.string "plaid_region", default: "us", null: false
+    t.string "institution_url"
+    t.string "institution_id"
+    t.string "institution_color"
     t.index ["family_id"], name: "index_plaid_items_on_family_id"
   end
 
