9d1511b922f23454b09f2f31f143d706af154523
Zach Gollwitzer <zach.gollwitzer@gmail.com>
2025-02-07 15:27:15 -0500

    Add currency column to all imports

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_02_06_003115) do
+ActiveRecord::Schema[7.2].define(version: 2025_02_06_204404) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
