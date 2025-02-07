536c82f2aab2f0fb9a3a92a151f01528f762e251
Zach Gollwitzer <zach@maybe.co>
2025-02-07 15:36:05 -0500

    Feature: Add the ability to "revert" a CSV import (#1814)
    * Allow reverting imports
    
    * Fix tests
    
    * Add currency column to all imports
    
    * Don't auto-enrich demo account

--- app/models/import.rb
+++ app/models/import.rb
@@ -6,7 +6,14 @@ class Import < ApplicationRecord
 
   scope :ordered, -> { order(created_at: :desc) }
 
-  enum :status, { pending: "pending", complete: "complete", importing: "importing", failed: "failed" }, validate: true
+  enum :status, {
+    pending: "pending",
+    complete: "complete",
+    importing: "importing",
+    reverting: "reverting",
+    revert_failed: "revert_failed",
+    failed: "failed"
+  }, validate: true, default: "pending"
 
   validates :type, inclusion: { in: TYPES }
   validates :col_sep, inclusion: { in: [ ",", ";" ] }
@@ -33,6 +40,27 @@ class Import < ApplicationRecord
     update! status: :complete
   rescue => error
     update! status: :failed, error: error.message
+  end
+
+  def revert_later
+    raise "Import is not revertable" unless revertable?
+
+    update! status: :reverting
+
+    RevertImportJob.perform_later(self)
+  end
+
+  def revert
+    Import.transaction do
+      accounts.destroy_all
+      entries.destroy_all
+    end
+
+    family.sync
+
+    update! status: :pending
+  rescue => error
+    update! status: :revert_failed, error: error.message
   end
 
   def csv_rows
@@ -111,6 +139,10 @@ class Import < ApplicationRecord
 
   def publishable?
     cleaned? && mappings.all?(&:valid?)
+  end
+
+  def revertable?
+    complete? || revert_failed?
   end
 
   def has_unassigned_account?
