d31d5c54673a3d7269e78e3f103a16fdcdf3ad1a
Marvin M <39344769+M123-dev@users.noreply.github.com>
2025-02-11 21:18:22 +0100

    fix: liabilities favorable direction is "down" (#1849)

--- app/models/family.rb
+++ app/models/family.rb
@@ -115,7 +115,7 @@ class Family < ApplicationRecord
 
     {
       asset_series: TimeSeries.new(result.map { |r| { date: r.date, value: Money.new(r.assets, r.currency) } }),
-      liability_series: TimeSeries.new(result.map { |r| { date: r.date, value: Money.new(r.liabilities, r.currency) } }),
+      liability_series: TimeSeries.new(result.map { |r| { date: r.date, value: Money.new(r.liabilities, r.currency) } }, favorable_direction: "down"),
       net_worth_series: TimeSeries.new(result.map { |r| { date: r.date, value: Money.new(r.net_worth, r.currency) } })
     }
   end
