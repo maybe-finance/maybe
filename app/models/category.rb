c620d1fc1e53aea2f0db6388e67590485b2fd6d7
Tony Vincent <tonyvince7@gmail.com>
2025-02-20 15:03:30 +0100

    fix: Ghost subcategories when parent category is deleted (#1874)

--- app/models/category.rb
+++ app/models/category.rb
@@ -5,7 +5,7 @@ class Category < ApplicationRecord
   belongs_to :family
 
   has_many :budget_categories, dependent: :destroy
-  has_many :subcategories, class_name: "Category", foreign_key: :parent_id
+  has_many :subcategories, class_name: "Category", foreign_key: :parent_id, dependent: :nullify
   belongs_to :parent, class_name: "Category", optional: true
 
   validates :name, :color, :family, presence: true
