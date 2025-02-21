4f8694da92b4551a35b5f34e0845bb26013a2799
David Anyatonwu <davidanyatonwu@gmail.com>
2025-02-21 15:11:47 +0000

    Revert changes to existing migration

--- db/migrate/20250207194638_adjust_securities_indexes.rb
+++ db/migrate/20250207194638_adjust_securities_indexes.rb
@@ -1,9 +1,6 @@
 class AdjustSecuritiesIndexes < ActiveRecord::Migration[7.2]
   def change
-    # Add back the original index that was removed
-    add_index :securities, [ :ticker, :exchange_mic ], unique: true, name: "index_securities_on_ticker_and_exchange_mic"
-
-    # Add the new index for exchange_operating_mic
+    remove_index :securities, name: "index_securities_on_ticker_and_exchange_mic"
     add_index :securities, [ :ticker, :exchange_operating_mic ], unique: true
   end
 end
