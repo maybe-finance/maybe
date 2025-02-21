890f4f6795af0b246bd92b7e728fe75220ec0c99
David Anyatonwu <davidanyatonwu@gmail.com>
2025-02-21 15:14:38 +0000

    Simplify migration to use change method

--- db/migrate/20250220153958_update_imports_for_operating_mic_v2.rb
+++ db/migrate/20250220153958_update_imports_for_operating_mic_v2.rb
@@ -1,25 +1,6 @@
 class UpdateImportsForOperatingMicV2 < ActiveRecord::Migration[7.2]
-  def up
-    # First remove the old exchange columns if they exist
-    remove_column :import_rows, :exchange if column_exists?(:import_rows, :exchange)
-    remove_column :imports, :exchange_col_label if column_exists?(:imports, :exchange_col_label)
-
-    # Then remove and re-add the operating mic columns to ensure they're in the correct state
-    remove_column :import_rows, :exchange_operating_mic if column_exists?(:import_rows, :exchange_operating_mic)
-    remove_column :imports, :exchange_operating_mic_col_label if column_exists?(:imports, :exchange_operating_mic_col_label)
-
-    # Add the columns fresh
+  def change
     add_column :import_rows, :exchange_operating_mic, :string
     add_column :imports, :exchange_operating_mic_col_label, :string
-  end
-
-  def down
-    # Remove the new columns
-    remove_column :import_rows, :exchange_operating_mic
-    remove_column :imports, :exchange_operating_mic_col_label
-
-    # Add back the old columns
-    add_column :import_rows, :exchange, :string
-    add_column :imports, :exchange_col_label, :string
   end
 end
