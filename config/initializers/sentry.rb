e8390a68d85bfb006a3cd56545daa4fece0b81c4
Josh Pigford <josh@joshpigford.com>
2025-02-25 08:44:13 -0600

    Reduce Sentry sampling rates for performance monitoring

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -8,12 +8,12 @@ if ENV["SENTRY_DSN"].present?
     # Set traces_sample_rate to 1.0 to capture 100%
     # of transactions for performance monitoring.
     # We recommend adjusting this value in production.
-    config.traces_sample_rate = 0.5
+    config.traces_sample_rate = 0.25
 
     # Set profiles_sample_rate to profile 100%
     # of sampled transactions.
     # We recommend adjusting this value in production.
-    config.profiles_sample_rate = 0.5
+    config.profiles_sample_rate = 0.25
 
     config.profiler_class = Sentry::Vernier::Profiler
   end
