d6793dec0507ea50dc6b26be59a65983de5d63b6
Zach Gollwitzer <zach@maybe.co>
2025-02-28 08:36:57 -0500

    Fix import configuration form so number format is applied (#1923)
    * Fix number format form error when loading import
    
    * Add test to verify import configuration was properly applied

--- test/controllers/import/configurations_controller_test.rb
+++ test/controllers/import/configurations_controller_test.rb
@@ -23,11 +23,24 @@ class Import::ConfigurationsControllerTest < ActionDispatch::IntegrationTest
         tags_col_label: "Tags",
         amount_col_label: "Amount",
         signage_convention: "inflows_positive",
-        account_col_label: "Account"
+        account_col_label: "Account",
+        number_format: "1.234,56"
       }
     }
 
     assert_redirected_to import_clean_url(@import)
     assert_equal "Import configured successfully.", flash[:notice]
+
+    # Verify configurations were saved
+    @import.reload
+    assert_equal "Date", @import.date_col_label
+    assert_equal "%Y-%m-%d", @import.date_format
+    assert_equal "Name", @import.name_col_label
+    assert_equal "Category", @import.category_col_label
+    assert_equal "Tags", @import.tags_col_label
+    assert_equal "Amount", @import.amount_col_label
+    assert_equal "inflows_positive", @import.signage_convention
+    assert_equal "Account", @import.account_col_label
+    assert_equal "1.234,56", @import.number_format
   end
 end
