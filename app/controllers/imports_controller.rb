c44d5297041e80d86e591f1efbbb64a0c6c96415
Zach Gollwitzer <zach.gollwitzer@gmail.com>
2025-03-03 10:44:51 -0500

    Initialize import directly from accounts page

--- app/controllers/imports_controller.rb
+++ app/controllers/imports_controller.rb
@@ -18,7 +18,8 @@ class ImportsController < ApplicationController
   end
 
   def create
-    import = Current.family.imports.create! import_params
+    account = Current.family.accounts.find_by(id: import_params[:account_id])
+    import = Current.family.imports.create!(type: import_params[:type], account: account)
 
     redirect_to import_upload_path(import)
   end
@@ -48,6 +49,6 @@ class ImportsController < ApplicationController
     end
 
     def import_params
-      params.require(:import).permit(:type)
+      params.require(:import).permit(:type, :account_id)
     end
 end
