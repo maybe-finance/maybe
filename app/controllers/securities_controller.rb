cf59fe45e7798cacacd3ffe913a5dfca921a0c9b
Zach Gollwitzer <zach@maybe.co>
2025-03-05 09:30:47 -0500

    Fix ticker filling when Synth is connected (#1950)

--- app/controllers/securities_controller.rb
+++ app/controllers/securities_controller.rb
@@ -1,10 +1,7 @@
 class SecuritiesController < ApplicationController
   def index
-    query = params[:q]
-    return render json: [] if query.blank? || query.length < 2 || query.length > 100
-
     @securities = Security.search_provider({
-      search: query,
+      search: params[:q],
       country: params[:country_code] == "US" ? "US" : nil
     })
   end
