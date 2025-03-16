54878e97021391abd454ba17910937992f726979
Zach Gollwitzer <zach.gollwitzer@gmail.com>
2025-03-16 06:39:28 -0400

    Update money to use new find rates method

--- app/models/exchange_rate/provided.rb
+++ app/models/exchange_rate/provided.rb
@@ -6,6 +6,17 @@ module ExchangeRate::Provided
       Providers.synth
     end
 
+    def find_or_fetch_rate(from:, to:, date: Date.current, cache: true)
+      rate = find_by(from_currency: from, to_currency: to, date: date)
+      return rate if rate.present?
+
+      response = provider.fetch_exchange_rate(from: from, to: to, date: date)
+
+      rate = response.data.rate
+      rate.save! if cache
+      rate
+    end
+
     def sync_provider_rates(from:, to:, start_date:, end_date: Date.current)
       # TODO
     end
