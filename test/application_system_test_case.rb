7682480d8726580ad4cd7a4a6fbde89c1784a277
Zach Gollwitzer <zach.gollwitzer@gmail.com>
2025-04-10 17:20:05 -0400

    Change test password to avoid chromium conflicts

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -13,7 +13,7 @@ class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
       visit new_session_path
       within "form" do
         fill_in "Email", with: user.email
-        fill_in "Password", with: "password"
+        fill_in "Password", with: "maybetestpassword817983172"
         click_on "Log in"
       end
 
