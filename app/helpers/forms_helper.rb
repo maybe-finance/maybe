694211411089c062f923de8ac10e631c5019327d
oliver <8559757+oliverqx@users.noreply.github.com>
2025-04-10 23:36:30 -0600

    feat(radio_tab_contents): make icon responsive

--- app/helpers/forms_helper.rb
+++ app/helpers/forms_helper.rb
@@ -31,7 +31,7 @@ end
   private
     def radio_tab_contents(label:, icon:)
       tag.div(class: "flex px-4 py-1 rounded-lg items-center space-x-2 justify-center text-subdued group-has-checked:bg-white group-has-checked:text-gray-800 group-has-checked:shadow-sm") do
-        concat lucide_icon(icon, class: "w-5 h-5")
+        concat lucide_icon(icon, class: "w-[clamp(15px,1.8vw,20px)] h-[clamp(15px,1.8vw,20px)]")
         concat tag.span(label, class: "group-has-checked:font-semibold text-[clamp(14px,1.5vw,16px)]")
       end
     end
