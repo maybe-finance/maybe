5a314538c53e2912ef4c000093aebc6d49c32be2
neo773 <neo773@protonmail.com>
2025-04-11 22:49:11 +0530

    WIP

--- app/javascript/controllers/password_visibility_controller.js
+++ app/javascript/controllers/password_visibility_controller.js
@@ -2,18 +2,19 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="password-visibility"
 export default class extends Controller {
-  static targets = ["input", "icon"]
+  static targets = ["input", "showIcon", "hideIcon"]
+
+  connect() {
+    // Initially hide the eye-off icon
+    this.hideIconTarget.classList.add("hidden")
+  }
 
   toggle() {
     const input = this.inputTarget
     const type = input.type === "password" ? "text" : "password"
     input.type = type
     
-    // Toggle icon
-    if (type === "password") {
-      this.iconTarget.dataset.icon = "eye"
-    } else {
-      this.iconTarget.dataset.icon = "eye-off"
-    }
+    this.showIconTarget.classList.toggle("hidden")
+    this.hideIconTarget.classList.toggle("hidden")
   }
 } 
