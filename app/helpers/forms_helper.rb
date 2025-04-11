d067fe27f89288a2448a7c46dbb5aa7aeb1ca935
Josh Pigford <josh@joshpigford.com>
2025-04-11 09:20:05 -0500

    More progress on dark mode

--- app/helpers/forms_helper.rb
+++ app/helpers/forms_helper.rb
@@ -17,7 +17,7 @@ module FormsHelper
     end
   end
 
-  def period_select(form:, selected:, classes: "border border-secondary rounded-lg text-sm pr-7 cursor-pointer text-primary focus:outline-hidden focus:ring-0")
+  def period_select(form:, selected:, classes: "border border-secondary bg-container-inset rounded-lg text-sm pr-7 cursor-pointer text-primary focus:outline-hidden focus:ring-0")
     periods_for_select = Period.all.map { |period| [ period.label_short, period.key ] }
 
     form.select(:period, periods_for_select, { selected: selected.key }, class: classes, data: { "auto-submit-form-target": "auto" })
