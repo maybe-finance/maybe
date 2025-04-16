665a478e4fc4747dce575884b476843e923b81e9
Luan Estradioto <luanestradioto@gmail.com>
2025-04-16 14:25:22 -0300

    better cleanup on address string

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,14 +2,17 @@ class Address < ApplicationRecord
   belongs_to :addressable, polymorphic: true
 
   def to_s
-    I18n.t("address.format",
+    string = I18n.t("address.format",
       line1: line1,
       line2: line2,
       county: county,
-      locality: locality.present? ? "#{locality}, " : "",
+      locality: locality,
       region: region,
       country: country,
       postal_code: postal_code
     )
+
+    # Clean up the string to maintain I18n comma formatting
+    string.split(",").map(&:strip).reject(&:empty?).join(", ")
   end
 end
