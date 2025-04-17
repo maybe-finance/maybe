6a21f26d2d006d8f2bd628a870cde87ab806b079
Luan Estradioto <luan@decorea.com.br>
2025-04-16 22:26:45 -0300

    Fix: No comma when locality is empty (small fix) (#2111)
    * Fix: No comma when locality is empty
    
    * better cleanup on address string
    
    * fix test to one-liner
    
    * add more testing

--- test/models/address_test.rb
+++ test/models/address_test.rb
@@ -10,6 +10,41 @@ class AddressTest < ActiveSupport::TestCase
       postal_code: "94101"
     )
 
-    assert_equal "123 Main St\n\nSan Francisco, CA 94101\nUS", address.to_s
+    assert_equal "123 Main St, San Francisco, CA 94101 US", address.to_s
+  end
+
+  test "can print a formatted address with line2" do
+    address = Address.new(
+      line1: "123 Main St",
+      line2: "Apt 1",
+      locality: "San Francisco",
+      region: "CA",
+      country: "US",
+      postal_code: "94101"
+    )
+
+    assert_equal "123 Main St Apt 1, San Francisco, CA 94101 US", address.to_s
+  end
+
+  test "can print empty when address is empty" do
+    address = Address.new(
+      line1: nil,
+      line2: nil,
+      locality: nil,
+      region: nil,
+      country: nil,
+      postal_code: nil
+    )
+
+    assert_equal "", address.to_s
+  end
+
+  test "can strip extras commas and spaces" do
+    address = Address.new(
+      line1: "123 Main St ,",
+      locality: " San Francisco, ",
+    )
+
+    assert_equal "123 Main St, San Francisco", address.to_s
   end
 end
