d1eef076f0945ee0d208fd38f53ac26a151f7e5e
hatz <hatz@hey.com>
2025-04-21 10:46:50 -0500

    Add nice formatting for subtypes on account list

--- app/helpers/accounts_helper.rb
+++ app/helpers/accounts_helper.rb
@@ -3,4 +3,26 @@ module AccountsHelper
     content = capture(&block)
     render "accounts/summary_card", title: title, content: content
   end
+  
+  # Format account subtype to be more friendly
+  def format_account_subtype(subtype)
+    return nil if subtype.nil?
+    
+    # Mapping of special cases
+    special_cases = {
+      "ira" => "IRA",
+      "401k" => "401(k)",
+      "hsa" => "HSA",
+    }
+    
+    # Convert to title case
+    subtype_string = subtype.titleize
+    
+    special_cases.each do |key, value|
+      # Use word boundaries to ensure we're replacing whole words
+      subtype_string = subtype_string.gsub(/\b#{key}\b/i, value)
+    end
+    
+    subtype_string
+  end
 end
