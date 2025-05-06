0fdeebceb10525574047bb77d89c398b82837b4b
Zach Gollwitzer <zach.gollwitzer@gmail.com>
2025-05-06 11:53:12 -0400

    Fix bulk edit dialog form structure

--- app/javascript/controllers/time_series_chart_controller.js
+++ app/javascript/controllers/time_series_chart_controller.js
@@ -57,8 +57,6 @@ export default class extends Controller {
       date_formatted: d.date_formatted,
       trend: d.trend,
     }));
-
-    console.log(this._normalDataPoints);
   }
 
   _rememberInitialContainerSize() {
