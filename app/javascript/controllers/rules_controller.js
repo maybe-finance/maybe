849e8202251519db43c211cfd3ba884e3e09af2f
hatz <hatz@hey.com>
2025-05-10 12:27:55 -0500

    Use data condition to update the prefix on conditions, condition groups, and subconditions

--- app/javascript/controllers/rules_controller.js
+++ app/javascript/controllers/rules_controller.js
@@ -48,13 +48,33 @@ export default class extends Controller {
     return Date.now();
   }
 
+  // Updates the prefix visibility of all conditions and condition groups
+  // This is also called by the rule/conditions_controller when a subcondition is removed
   updateConditionPrefixes() {
-    const items = this.conditionsListTarget.querySelectorAll('[data-condition-prefix]');
-    items.forEach((el, idx) => {
-      if (idx === 0) {
-        el.classList.add('hidden');
-      } else {
-        el.classList.remove('hidden');
+    // Update conditions
+    this.#updatePrefixesForList(this.conditionsListTarget);
+
+    // Update subconditions for each condition group
+    // We currently only support a single level of subconditions
+    const groupSubLists = this.conditionsListTarget.querySelectorAll('[data-rule--conditions-target="subConditionsList"]');
+    groupSubLists.forEach((subList) => {
+      this.#updatePrefixesForList(subList);
+    });
+  }
+
+  // Helper to update prefixes for a given list
+  #updatePrefixesForList(listEl) {
+    const items = Array.from(listEl.children);
+    let conditionIdx = 0;
+    items.forEach((item) => {
+      const prefixEl = item.querySelector('[data-condition-prefix]');
+      if (prefixEl) {
+        if (conditionIdx === 0) {
+          prefixEl.classList.add('hidden');
+        } else {
+          prefixEl.classList.remove('hidden');
+        }
+        conditionIdx++;
       }
     });
   }
