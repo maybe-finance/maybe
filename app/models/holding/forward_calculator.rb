64147758a3cc5bb0d053ef4f7902e4fc9b5ce043
Zach Gollwitzer <zach.gollwitzer@gmail.com>
2025-05-10 16:16:47 -0400

    Remove bad abstraction

--- app/models/holding/forward_calculator.rb
+++ app/models/holding/forward_calculator.rb
@@ -1,7 +1,63 @@
-class Holding::ForwardCalculator < Holding::BaseCalculator
+class Holding::ForwardCalculator
+  attr_reader :account
+
+  def initialize(account)
+    @account = account
+  end
+
+  def calculate
+    Rails.logger.tagged("Holding::ForwardCalculator") do
+      holdings = calculate_holdings
+      Holding.gapfill(holdings)
+    end
+  end
+
   private
     def portfolio_cache
       @portfolio_cache ||= Holding::PortfolioCache.new(account)
+    end
+
+    def empty_portfolio
+      securities = portfolio_cache.get_securities
+      securities.each_with_object({}) { |security, hash| hash[security.id] = 0 }
+    end
+
+    def generate_starting_portfolio
+      empty_portfolio
+    end
+
+    def transform_portfolio(previous_portfolio, trade_entries, direction: :forward)
+      new_quantities = previous_portfolio.dup
+
+      trade_entries.each do |trade_entry|
+        trade = trade_entry.entryable
+        security_id = trade.security_id
+        qty_change = trade.qty
+        qty_change = qty_change * -1 if direction == :reverse
+        new_quantities[security_id] = (new_quantities[security_id] || 0) + qty_change
+      end
+
+      new_quantities
+    end
+
+    def build_holdings(portfolio, date, price_source: nil)
+      portfolio.map do |security_id, qty|
+        price = portfolio_cache.get_price(security_id, date, source: price_source)
+
+        if price.nil?
+          next
+        end
+
+        Holding.new(
+          account_id: account.id,
+          security_id: security_id,
+          date: date,
+          qty: qty,
+          price: price.price,
+          currency: price.currency,
+          amount: qty * price.price
+        )
+      end.compact
     end
 
     def calculate_holdings
--- app/models/holding/reverse_calculator.rb
+++ app/models/holding/reverse_calculator.rb
@@ -1,4 +1,17 @@
-class Holding::ReverseCalculator < Holding::BaseCalculator
+class Holding::ReverseCalculator
+  attr_reader :account
+
+  def initialize(account)
+    @account = account
+  end
+
+  def calculate
+    Rails.logger.tagged("Holding::ReverseCalculator") do
+      holdings = calculate_holdings
+      Holding.gapfill(holdings)
+    end
+  end
+
   private
     # Reverse calculators will use the existing holdings as a source of security ids and prices
     # since it is common for a provider to supply "current day" holdings but not all the historical
@@ -25,6 +38,11 @@ class Holding::ReverseCalculator < Holding::BaseCalculator
       holdings
     end
 
+    def empty_portfolio
+      securities = portfolio_cache.get_securities
+      securities.each_with_object({}) { |security, hash| hash[security.id] = 0 }
+    end
+
     # Since this is a reverse sync, we start with today's holdings
     def generate_starting_portfolio
       holding_quantities = empty_portfolio
@@ -36,5 +54,39 @@ class Holding::ReverseCalculator < Holding::BaseCalculator
       end
 
       holding_quantities
+    end
+
+    def transform_portfolio(previous_portfolio, trade_entries, direction: :forward)
+      new_quantities = previous_portfolio.dup
+
+      trade_entries.each do |trade_entry|
+        trade = trade_entry.entryable
+        security_id = trade.security_id
+        qty_change = trade.qty
+        qty_change = qty_change * -1 if direction == :reverse
+        new_quantities[security_id] = (new_quantities[security_id] || 0) + qty_change
+      end
+
+      new_quantities
+    end
+
+    def build_holdings(portfolio, date, price_source: nil)
+      portfolio.map do |security_id, qty|
+        price = portfolio_cache.get_price(security_id, date, source: price_source)
+
+        if price.nil?
+          next
+        end
+
+        Holding.new(
+          account_id: account.id,
+          security_id: security_id,
+          date: date,
+          qty: qty,
+          price: price.price,
+          currency: price.currency,
+          amount: qty * price.price
+        )
+      end.compact
     end
 end
