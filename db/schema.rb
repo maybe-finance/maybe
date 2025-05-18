14067dd7b86936c5ccb63c1f3ad884010f47a1da
Cameron Roudebush <roudebushcameron@gmail.com>
2025-05-18 08:48:42 -0400

    chore: DB migration update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_05_09_182903) do
+ActiveRecord::Schema[7.2].define(version: 2025_05_17_134646) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
