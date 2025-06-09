9fabcf4c7253468143ab2288b5060713208e8b8a
Zach Gollwitzer <zach@maybe.co>
2025-06-09 18:30:52 -0400

    Redis check for self hosted apps (#2353)
    * Redis check for self hosted apps
    
    * Run linter with autocorrect
    
    * Add Redis to CI

--- app/controllers/concerns/self_hostable.rb
+++ app/controllers/concerns/self_hostable.rb
@@ -3,6 +3,8 @@ module SelfHostable
 
   included do
     helper_method :self_hosted?, :self_hosted_first_login?
+
+    prepend_before_action :verify_self_host_config
   end
 
   private
@@ -12,5 +14,30 @@ module SelfHostable
 
     def self_hosted_first_login?
       self_hosted? && User.count.zero?
+    end
+
+    def verify_self_host_config
+      return unless self_hosted?
+
+      # Special handling for Redis configuration error page
+      if controller_name == "pages" && action_name == "redis_configuration_error"
+        # If Redis is now working, redirect to home
+        if redis_connected?
+          redirect_to root_path, notice: "Redis is now configured properly! You can now setup your Maybe application."
+        end
+
+        return
+      end
+
+      unless redis_connected?
+        redirect_to redis_configuration_error_path
+      end
+    end
+
+    def redis_connected?
+      Redis.new.ping
+      true
+    rescue Redis::CannotConnectError
+      false
     end
 end
